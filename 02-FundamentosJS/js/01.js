0a28f33a1a834deb278ecd472d5a15c3f6d8ab5a
Adriano Ayala <adriano.ayc@gmail.com>
2021-12-20 00:09:56 -0400

    Variables con let

---
+++ 02-FundamentosJS/js/01.js
@@ -0,0 +1,19 @@
+// console.log('Hola Mundo JS!')           // El punto y coma es opcional en JS
+// console.log('Hola Mundo JS!');
+
+// Variables - var (no se utiliza en las sintaxis modernas), let y const
+let cliente = "Adriano";
+// Camelcase
+let precioDescuento = 220;
+
+// Puedes iniciar sin un valor
+let precio;
+precio = 1000;
+
+// Reacinar
+cliente = 20;
+clinete = true;
+
+console.log(cliente);           // Adriano (color negro)
+console.log(precioDescuento);   // 220 (color azul)
+console.log(precio);            // undefined | 1000
