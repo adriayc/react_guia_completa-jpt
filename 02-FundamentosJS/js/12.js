3578f20814d1e577c42a76edefeccb7c4fd4180c
Adriano Ayala <adriano.ayc@gmail.com>
2021-12-26 23:46:19 -0400

    Funciones - Function declaration

---
+++ 02-FundamentosJS/js/12.js
@@ -0,0 +1,18 @@
+// Funciones - Function Declaration
+
+// Llamar la funcion - Opcion #2 (tambien puede ser llamando antes que la funcion este declarado)
+// sumar();
+
+/**
+ * Funcion sumar
+ * El nombre debe ser descritivo
+ * Si tiene mas de dos palabras debe ser camelcase
+ */
+function sumar() {
+    console.log(2 + 2);
+}
+
+// Llamar la funcion - Opcion #1
+sumar();
+// sumar();             // Puede llamarse varias veces la funcion
+// sumar();
