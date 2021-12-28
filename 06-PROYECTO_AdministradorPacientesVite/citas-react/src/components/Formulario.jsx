736d894cdd2d14c0aaeb2ee557eb25175cc61738
Adriano Ayala <adriano.ayc@gmail.com>
2021-12-28 18:44:55 -0400

    Creando un Input en el Formulario

--- 06-PROYECTO_AdministradorPacientesVite/citas-react/src/components/Formulario.jsx
+++ 06-PROYECTO_AdministradorPacientesVite/citas-react/src/components/Formulario.jsx
@@ -4,7 +4,18 @@ const Formulario = () => {
         <div className="md:w-1/2 lg:w-2/5">
             <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
 
-            <p className="text-lg text-center mt-5">Añade Pacientes y <span className="text-indigo-600 font-bold">Administralos</span></p>
+            <p className="text-lg text-center mt-5 mb-10">Añade Pacientes y <span className="text-indigo-600 font-bold">Administralos</span></p>
+
+            <form className="bg-white shadow-md rounded-lg py-10 px-5">
+                <div>
+                    <label htmlFor="mascota" className="block text-gray-700 font-bold uppercase">Nombre Mascota</label>
+                    <input 
+                        type="text"
+                        id="mascota" 
+                        placeholder="Nombre de la Mascota" 
+                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" />
+                </div>
+            </form>
         </div>
     )
 }
