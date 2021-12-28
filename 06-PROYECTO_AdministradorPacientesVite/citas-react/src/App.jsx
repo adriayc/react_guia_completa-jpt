8843d99808f0269bf93fab53f7435aad1bf6dafa
Adriano Ayala <adriano.ayc@gmail.com>
2021-12-28 16:05:58 -0400

    Creando y llamando nuestro primer Componente

--- 06-PROYECTO_AdministradorPacientesVite/citas-react/src/App.jsx
+++ 06-PROYECTO_AdministradorPacientesVite/citas-react/src/App.jsx
@@ -1,38 +1,12 @@
+// Importar componentes
+import Header from './components/Header';
 
 function App() {
-  // Aqui codigo JS
-  // const sumar = () => {
-  //   // console.log(2 + 2);
 
-  //   const numero = 5;
-
-  //   if(numero > 5) {
-  //     console.log('Si es mayor a 5');
-  //   } else {
-  //     console.log('No es mayor a 5');
-  //   }
-  // };
-
-  // sumar();
-
-  const edad = 18;
-
-  // Debe existir un return
   return (
-    // Aqui codigo HTMl o expresiones de React
-
-    // Usando un Fragment como el elemento máximo
     <>
-      {/* Codigo JS */}
-      {/* { 1 + 1 } */}
-      { edad >= 18 ? 'Eres mayor de edad' : 'No eres mayor de edad' }
-
-      <div>
-        {/* <h1>{ 'Hola Mundo React!'.toLowerCase() }</h1> */}
-        {/* <h1>{ 'Hola Mundo React!'.toUpperCase() }</h1> */}
-        <h1>{ edad }</h1>
-        <p>Un parrafo</p>
-      </div>
+      {/* Llamar el componente */}
+      <Header />
     </>
   )
 }
