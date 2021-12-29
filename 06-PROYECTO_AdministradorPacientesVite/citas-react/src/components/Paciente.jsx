68ace48b7ddc394f7c08a84e566ce2ac3569fc8a
Adriano Ayala <adriano.ayc@gmail.com>
2021-12-28 21:55:47 -0400

    Creando el Listado de Pacientes

---
+++ 06-PROYECTO_AdministradorPacientesVite/citas-react/src/components/Paciente.jsx
@@ -0,0 +1,33 @@
+
+const Paciente = () => {
+    return (
+        <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+            <p className="font-bold mb-3 text-gray-700 uppercase">
+                Nombre: {''} 
+                <span className="font-normal normal-case">Hook</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">
+                Propietario: {''} 
+                <span className="font-normal normal-case">Adriano</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">
+                Email: {''} 
+                <span className="font-normal normal-case">adriano@correo.com</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">
+                Fecha Alta: {''} 
+                <span className="font-normal normal-case">10 Diciembre de 2021</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">
+                Síntomas: {''} 
+                <span className="font-normal normal-case">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis aspernatur impedit accusamus repellat nemo architecto rem! Magnam, aspernatur sint at maiores iure ipsam ratione. Natus laborum in, sapiente fugiat obcaecati neque quidem eaque minima ut accusamus nam optio voluptate, labore incidunt nobis quo esse. Nesciunt quisquam natus adipisci vel placeat?</span>
+            </p>
+        </div>
+    )
+}
+
+export default Paciente
