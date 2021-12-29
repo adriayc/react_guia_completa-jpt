ee5cc341b7966530a7b0f14fb42b05783b08b2bc
Adriano Ayala <adriano.ayc@gmail.com>
2021-12-28 23:17:56 -0400

    Leyendo el Resto de los Inputs y colocandolos en el State

--- 06-PROYECTO_AdministradorPacientesVite/citas-react/src/components/Formulario.jsx
+++ 06-PROYECTO_AdministradorPacientesVite/citas-react/src/components/Formulario.jsx
@@ -4,6 +4,10 @@ import { useState, useEffect } from 'react';
 const Formulario = () => {
     // Declaramos un state con un valor inicial (Debe estar dentro del componente)
     const [ nombre, setNombre ] = useState('');
+    const [ propietario, setPropietario ] = useState('');
+    const [ email, setEmail ] = useState('');
+    const [ fecha, setFecha ] = useState('');
+    const [ sintomas, setSintomas ] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -11,7 +15,7 @@ const Formulario = () => {
     };
 
     return (
-        <div className="md:w-1/2 lg:w-2/5">
+        <div className="md:w-1/2 lg:w-2/5 mx-5">
             <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
 
             <p className="text-lg text-center mt-5 mb-10">Añade Pacientes y <span className="text-indigo-600 font-bold">Administralos</span></p>
@@ -39,7 +43,10 @@ const Formulario = () => {
                         type="text"
                         id="propietario" 
                         placeholder="Nombre del Propietario" 
-                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" />
+                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        value={propietario}
+                        onChange={(e) => setPropietario(e.target.value)} 
+                    />
                 </div>
 
                 <div className="mb-5">
@@ -48,7 +55,10 @@ const Formulario = () => {
                         type="email"
                         id="email" 
                         placeholder="Email Contacto Propietario" 
-                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" />
+                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)} 
+                    />
                 </div>
 
                 <div className="mb-5">
@@ -56,7 +66,10 @@ const Formulario = () => {
                     <input 
                         type="date"
                         id="alta" 
-                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" />
+                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
+                        value={fecha}
+                        onChange={(e) => setFecha(e.target.value)} 
+                    />
                 </div>
 
                 <div className="mb-5">
@@ -64,7 +77,10 @@ const Formulario = () => {
                     <textarea  
                         id="sintomas" 
                         className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-                        placeholder="Describe los Síntomas"></textarea>
+                        placeholder="Describe los Síntomas"
+                        value={sintomas}
+                        onChange={(e) => setSintomas(e.target.value)} 
+                    ></textarea>
                 </div>
 
                 <input 
