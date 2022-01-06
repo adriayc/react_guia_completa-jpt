b28ffd31a1cc946841e1d4d1abc7871a03a90b1a
Adriano Ayala <adriano.ayc@gmail.com>
2022-01-06 15:21:40 -0400

    Mostrar Textos de Forma condicional en React

--- 06-PROYECTO_AdministradorPacientesVite/citas-react/src/components/ListadoPacientes.jsx
+++ 06-PROYECTO_AdministradorPacientesVite/citas-react/src/components/ListadoPacientes.jsx
@@ -1,26 +1,38 @@
 import Paciente from './Paciente';
 
 const ListadoPacientes = ({ pacientes }) => {
-    console.log(pacientes)
+    // console.log(pacientes && pacientes.length === 0);
 
     return (
         <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
-            <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
-            <p className="text-xl mt-5 mb-10 text-center">
-                Administra tus {''} 
-                <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
-            </p>
+            { pacientes && pacientes.length ? (
+                <>
+                    <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+                    <p className="text-xl mt-5 mb-10 text-center">
+                        Administra tus {''} 
+                        <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
+                    </p>
 
-            {/* Al iterar los pacientes react necesita de un Key unico */}
-            { pacientes.map(paciente => (
-                // <h1>Desde map</h1>
-                // <h1>{ paciente.nombre }</h1>
+                    {/* Al iterar los pacientes react necesita de un Key unico */}
+                    { pacientes.map(paciente => (
+                        // <h1>Desde map</h1>
+                        // <h1>{ paciente.nombre }</h1>
 
-                <Paciente 
-                    key={ paciente.id }
-                    paciente={ paciente } 
-                />
-            ))}
+                        <Paciente 
+                            key={ paciente.id }
+                            paciente={ paciente } 
+                        />
+                    ))}
+                </>
+            ) : (
+                <>
+                    <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
+                    <p className="text-xl mt-5 mb-10 text-center">
+                        Comienza agregado pacientes {''} 
+                        <span className="text-indigo-600 font-bold">y aparecerán en este lugar</span>
+                    </p>
+                </>
+            ) }
         </div>
     )
 }
