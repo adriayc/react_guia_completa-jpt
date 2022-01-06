3708778546ff3fc71837a638b6ce509367aeb088
Adriano Ayala <adriano.ayc@gmail.com>
2022-01-05 23:16:02 -0400

    Iterando sobre un Array en React

--- 06-PROYECTO_AdministradorPacientesVite/citas-react/src/components/ListadoPacientes.jsx
+++ 06-PROYECTO_AdministradorPacientesVite/citas-react/src/components/ListadoPacientes.jsx
@@ -1,6 +1,8 @@
 import Paciente from './Paciente';
 
-const ListadoPacientes = () => {
+const ListadoPacientes = ({ pacientes }) => {
+    console.log(pacientes)
+
     return (
         <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
             <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
@@ -9,12 +11,12 @@ const ListadoPacientes = () => {
                 <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
             </p>
 
-            <Paciente />
-            <Paciente />
-            <Paciente />
-            <Paciente />
-            <Paciente />
-            <Paciente />
+            { pacientes.map(paciente => (
+                // <h1>Desde map</h1>
+                // <h1>{ paciente.nombre }</h1>
+
+                <Paciente paciente={ paciente } />
+            ))}
         </div>
     )
 }
