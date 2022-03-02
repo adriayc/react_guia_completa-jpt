7b946b474c45bfa922b4e40e07fb1dd0d59580db
Adriano Ayala <adriano.ayc@gmail.com>
2022-03-02 11:23:27 -0400

    Añadir un boton para registrar un nuevo gasto

--- 07-PROYECTO_ControlPresupuestoReactViteLocalStorage/control-gastos/src/App.jsx
+++ 07-PROYECTO_ControlPresupuestoReactViteLocalStorage/control-gastos/src/App.jsx
@@ -3,6 +3,9 @@ import { useState } from 'react'
 // Importar componentes
 import Header from './components/Header';
 
+// Importar un imagen
+import IconoNuevoGasto from './img/nuevo-gasto.svg';
+
 function App() {
   const [presupuesto, setPresupuesto] = useState(0);
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
@@ -15,6 +18,22 @@ function App() {
         isValidPresupuesto={isValidPresupuesto}
         setIsValidPresupuesto={setIsValidPresupuesto}
       />
+
+      {/* Operador ternario */}
+      {/* {isValidPresupuesto ? (
+        <div className='nuevo-gasto'>
+          <img src={IconoNuevoGasto} alt="Icono nuevo gasto" />
+        </div>
+      ): null} */}
+
+      {isValidPresupuesto && (
+        <div className='nuevo-gasto'>
+          <img 
+            src={IconoNuevoGasto} 
+            alt="Icono nuevo gasto" 
+          />
+        </div>
+      )}
     </div>
   )
 }
