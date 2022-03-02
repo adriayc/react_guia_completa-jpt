3ae31c83ca61c52e91ce6de09e9699d2561646c6
Adriano Ayala <adriano.ayc@gmail.com>
2022-03-02 10:58:31 -0400

    Formatear Números a Dinero

--- 07-PROYECTO_ControlPresupuestoReactViteLocalStorage/control-gastos/src/components/ControlPresupuesto.jsx
+++ 07-PROYECTO_ControlPresupuestoReactViteLocalStorage/control-gastos/src/components/ControlPresupuesto.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 
 const ControlPresupuesto = ({ presupuesto }) => {
+  // Formatea el presupuesto (con una funcion propia de JS)
+  const formatearCantidad = (cantidad) => {
+    return cantidad.toLocaleString('en-US', {
+        style: 'currency',
+        currency: 'USD',
+    });
+  };
+
   return (
     <div className='contenedor contenedor-presupuesto sombra dos-columnas'>
         <div>
@@ -9,7 +17,13 @@ const ControlPresupuesto = ({ presupuesto }) => {
 
         <div className='contenido-presupuesto'>
             <p>
-                <span>Presupuesto: </span>${presupuesto}
+                <span>Presupuesto: </span>{formatearCantidad(presupuesto)}
+            </p>
+            <p>
+                <span>Disponible: </span>{formatearCantidad(0)}
+            </p>
+            <p>
+                <span>Gastado: </span>{formatearCantidad(0)}
             </p>
         </div>
     </div>
