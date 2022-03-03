2a442ee4933439c8519a891c9dbb97fbafa3547e
Adriano Ayala <adriano.ayc@gmail.com>
2022-03-03 18:39:32 -0400

    Formateando las Fechas

--- 07-PROYECTO_ControlPresupuestoReactViteLocalStorage/control-gastos/src/components/Gasto.jsx
+++ 07-PROYECTO_ControlPresupuestoReactViteLocalStorage/control-gastos/src/components/Gasto.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
 
+// Importar helpers
+import { formatearFecha } from '../helpers';
+
 const Gasto = ({ gasto }) => {
-  const { nombre, cantidad, categoria, id } = gasto;
+  const { nombre, cantidad, categoria, id, fecha } = gasto;
 
   return (
     <div className='gasto sombra'>
         <div className='contenido-gasto'>
+            {/* Imagen aquí */}
+
             <div className='descripcion-gasto'>
                 {/* <p className='categoria'>{gasto.categoria}</p> */}
                 <p className='categoria'>{categoria}</p>
                 <p className='nombre-gasto'>{nombre}</p>
+                <p className='fecha-gasto'>
+                    Agregado el: {''}
+                    <span>{formatearFecha(fecha)}</span>
+                </p>
             </div>
         </div>
+        <p className='cantidad-gasto'>${cantidad}</p>
     </div>
   )
 }
