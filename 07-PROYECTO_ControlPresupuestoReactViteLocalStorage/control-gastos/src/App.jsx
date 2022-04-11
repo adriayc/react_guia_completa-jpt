fe8cbffefe745b572d8df800109eab47fcfe9fe8
Adriano Ayala <adriano.ayc@gmail.com>
2022-04-10 23:32:38 -0400

    Mostrando Gastos Filtrados

--- 07-PROYECTO_ControlPresupuestoReactViteLocalStorage/control-gastos/src/App.jsx
+++ 07-PROYECTO_ControlPresupuestoReactViteLocalStorage/control-gastos/src/App.jsx
@@ -150,6 +150,8 @@ function App() {
               gastos={gastos}
               setGastoEditar={setGastoEditar}
               eliminarGasto={eliminarGasto}
+              filtro={filtro}
+              gastosFiltrados={gastosFiltrados}
             />
           </main>
           <div className='nuevo-gasto'>
--- 07-PROYECTO_ControlPresupuestoReactViteLocalStorage/control-gastos/src/components/ListadoGastos.jsx
+++ 07-PROYECTO_ControlPresupuestoReactViteLocalStorage/control-gastos/src/components/ListadoGastos.jsx
@@ -3,19 +3,34 @@ import React from 'react'
 // Importar componentes
 import Gasto from './Gasto';
 
-const ListadoGastos = ({ gastos, setGastoEditar, eliminarGasto }) => {
+const ListadoGastos = ({ gastos, setGastoEditar, eliminarGasto, filtro, gastosFiltrados }) => {
   return (
     <div className='contenedor listado-gastos'>
-        <h2>{gastos.length ? 'Gastos' : 'No hay gastos aún'}</h2>
-
-        {gastos.map(gasto => (
-          <Gasto 
-            key={gasto.id}        // Eliminamos el error de solicitud del 'key'
-            gasto={gasto}
-            setGastoEditar={setGastoEditar}
-            eliminarGasto={eliminarGasto}
-          />
-        ))}
+        {filtro ? (
+          <>
+            <h2>{gastosFiltrados.length ? 'Gastos' : 'No hay gastos aún'}</h2>
+            {gastosFiltrados.map(gasto => (
+              <Gasto 
+                key={gasto.id}        // Eliminamos el error de solicitud del 'key'
+                gasto={gasto}
+                setGastoEditar={setGastoEditar}
+                eliminarGasto={eliminarGasto}
+              />
+            ))}
+          </>
+        ) : (
+          <>
+            <h2>{gastos.length ? 'Gastos' : 'No hay gastos aún'}</h2>
+            {gastos.map(gasto => (
+              <Gasto 
+                key={gasto.id}        // Eliminamos el error de solicitud del 'key'
+                gasto={gasto}
+                setGastoEditar={setGastoEditar}
+                eliminarGasto={eliminarGasto}
+              />
+            ))}
+          </>
+        )}
     </div>
   )
 }
