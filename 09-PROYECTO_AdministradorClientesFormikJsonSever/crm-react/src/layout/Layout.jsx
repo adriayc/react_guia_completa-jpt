da6487d963c957bba4e637418003aa0180bbe179
Adriano Ayala <adriano.ayc@gmail.com>
2022-05-12 18:10:14 -0400

    El Hook useLocation()

--- 09-PROYECTO_AdministradorClientesFormikJsonSever/crm-react/src/layout/Layout.jsx
+++ 09-PROYECTO_AdministradorClientesFormikJsonSever/crm-react/src/layout/Layout.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet, Link, useLocation } from 'react-router-dom';
 
 const Layout = () => {
+  // Hook que muestra el nombre de la ruta que nos encontremos
+  const location = useLocation();
+  // console.log(location);
+  const urlActual = location.pathname;
+  // console.log(urlActual);
+
   return (
     <div className='md:flex md:min-h-screen'>
         <div className='md:w-1/4 bg-blue-900 px-5 py-10'>
@@ -9,15 +15,15 @@ const Layout = () => {
 
           <nav className='mt-10'>
             <Link 
-              className='text-white text-2xl block mt-2 hover:text-blue-300' 
+              className={`${urlActual === '/clientes' ? 'text-blue-300' : 'text-white'} text-2xl block mt-2 hover:text-blue-300`} 
               to="/clientes"
               >Clientes</Link>
             <Link 
-              className='text-white text-2xl block mt-2 hover:text-blue-300' 
+              className={`${urlActual === '/clientes/nuevo' ? 'text-blue-300' : 'text-white'} text-2xl block mt-2 hover:text-blue-300`} 
               to="/clientes/nuevo"
               >Nuevo Cliente</Link>
           </nav>
-        </div>
+        </div>  
 
         <div className='md:w-3/4'>
           {/* Outlet - injecta el contendio del componente hijo */}
