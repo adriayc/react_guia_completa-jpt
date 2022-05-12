2b2575695bca69124abd24b9914ca55cf8ef7eee
Adriano Ayala <adriano.ayc@gmail.com>
2022-05-12 12:11:53 -0400

    Agrupando Rutas y Definiendo Layout Principal con React Router 6

--- 09-PROYECTO_AdministradorClientesFormikJsonSever/crm-react/src/App.jsx
+++ 09-PROYECTO_AdministradorClientesFormikJsonSever/crm-react/src/App.jsx
@@ -2,23 +2,21 @@ import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
 
 // Importar componentes
-import IniciarSesion from './layout/IniciarSesion';
-import LoginForm from './paginas/LoginForm';
 import Layout from './layout/Layout';
 import Inicio from './paginas/Inicio';
+import NuevoCliente from './paginas/NuevoCliente';
+import EditarCliente from './paginas/EditarCliente';
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<IniciarSesion />}>    {/* Master page */}
-          <Route index element={<LoginForm />} />
-        </Route>
-
         {/* Nested Routes */}
         <Route path='/clientes' element={<Layout />}>
           <Route index element={<Inicio />} />
+          <Route path='nuevo' element={<NuevoCliente />} />
+          <Route path='editar/:id' element={<EditarCliente />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- 09-PROYECTO_AdministradorClientesFormikJsonSever/crm-react/src/paginas/LoginForm.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const LoginForm = () => {
-  return (
-    <div>Desde LoginForm JSX</div>
-  )
-}
-
-export default LoginForm
