b260e707893d5d96245308e4476841365dc3c405
Adriano Ayala <adriano.ayc@gmail.com>
2022-05-12 12:23:50 -0400

    Aplicando Tailwind al Layout Principal

--- 09-PROYECTO_AdministradorClientesFormikJsonSever/crm-react/src/layout/Layout.jsx
+++ 09-PROYECTO_AdministradorClientesFormikJsonSever/crm-react/src/layout/Layout.jsx
@@ -3,13 +3,26 @@ import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
   return (
-    <div>
-        <h1>Desde Layout JSX</h1>
+    <div className='md:flex md:min-h-screen'>
+        <div className='md:w-1/4 bg-blue-900 px-5 py-10'>
+          <h2 className='text-4xl font-black text-center text-white'>CRM - Clientes</h2>
 
-        {/* <p>Antes del Outlet</p> */}
-        {/* Outlet - injecta el contendio del componente hijo */}
-        <Outlet />
-        {/* <p>Despues del Outlet</p> */}
+          <nav className='mt-10'>
+            <a 
+              className='text-white text-2xl block mt-2 hover:text-blue-300' 
+              href="/clientes"
+              >Clientes</a>
+            <a 
+              className='text-white text-2xl block mt-2 hover:text-blue-300' 
+              href="/clientes/nuevo"
+              >Nuevo Cliente</a>
+          </nav>
+        </div>
+
+        <div className='md:w-3/4'>
+          {/* Outlet - injecta el contendio del componente hijo */}
+          <Outlet />
+        </div>
     </div>
   )
 }
