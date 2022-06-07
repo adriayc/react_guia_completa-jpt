b6e85c00a2b5ff5c9629849220fba10d3fbed106
Adriano Ayala <adriano.ayc@gmail.com>
2022-06-07 10:10:02 -0400

    Consultar la API para obtener los Clientes

--- 09-PROYECTO_AdministradorClientesFormikJsonSever/crm-react/src/paginas/Inicio.jsx
+++ 09-PROYECTO_AdministradorClientesFormikJsonSever/crm-react/src/paginas/Inicio.jsx
@@ -1,6 +1,30 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 
 const Inicio = () => {
+
+  const [clientes, setClientes] = useState([]);
+
+  // Definimos un useEffect, [] array vacio para que se ejecute una sola vez
+  useEffect(() => {
+    const obtenerClienteAPI = async () => {
+      try {
+        const url = 'http://localhost:4000/clientes';
+
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+        console.log(resultado);
+
+        setClientes(resultado);
+
+      } catch(error) {
+        console.log(error);
+      }
+    };
+
+    // Llamar al funcion
+    obtenerClienteAPI();
+  }, []);
+
   return (
     <div>Desde Inicio JSX</div>
   )
