e82a65100e154bf22e81b4a8ea6a19cffc6b5e1c
Adriano Ayala <adriano.ayc@gmail.com>
2022-06-07 15:25:35 -0400

    Mostrando la Informacion de un Cliente por su ID

--- 09-PROYECTO_AdministradorClientesFormikJsonSever/crm-react/src/paginas/VerCliente.jsx
+++ 09-PROYECTO_AdministradorClientesFormikJsonSever/crm-react/src/paginas/VerCliente.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 const VerCliente = () => {
+
+  const [cliente, setCliente] = useState({});
 
   // Obtenemos un objeto con el valor del parametro id
   // const params = useParams();
@@ -9,9 +11,60 @@ const VerCliente = () => {
   const { id } = useParams();
   console.log(id);
 
+  // Se ejecuta solo una vez
+  useEffect(() => {
+    const obtenerClienteAPI = async () => {
+      try {
+        const url = `http://localhost:4000/clientes/${id}`;
+
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+        console.log(resultado);
+
+        // Agregando el objeto cliente
+        setCliente(resultado);
+
+      } catch(error) {
+        console.log(error);
+      }
+    }
+
+    // Llamar el metodo
+    obtenerClienteAPI();
+
+  }, []);
   
   return (
-    <div>VerCliente</div>
+    <div>
+      <h1 className='font-black text-4xl text-blue-900'>Ver Cliente: {cliente.nombre}</h1>
+      <p className='mt-3'>Información del Cliente</p>
+
+      {cliente.nombre && (
+        <p className='text-4xl text-gray-600 mt-10'>
+          <span className='text-gray-800 uppercase font-bold'>Cliente: </span>{cliente.nombre}
+        </p>
+      )}
+      {cliente.email && (
+        <p className='text-2xl text-gray-600 mt-4'>
+          <span className='text-gray-800 uppercase font-bold'>Email: </span>{cliente.email}
+        </p>
+      )}
+      {cliente.telefono && (
+        <p className='text-2xl text-gray-600 mt-4'>
+          <span className='text-gray-800 uppercase font-bold'>Teléfono: </span>{cliente.telefono}
+        </p>
+      )}
+      {cliente.empresa &&(
+        <p className='text-2xl text-gray-600 mt-4'>
+          <span className='text-gray-800 uppercase font-bold'>Empresa: </span>{cliente.empresa}
+        </p>
+      )}
+      {cliente.notas && (
+        <p className='text-2xl text-gray-600 mt-4'>
+          <span className='text-gray-800 uppercase font-bold'>Notas: </span>{cliente.notas}
+        </p>
+      )}
+    </div>
   )
 }
 
