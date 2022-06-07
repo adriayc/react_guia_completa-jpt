daf4b4fa9dbf8ad1a3dc987800d7d0d5bcd9bb23
Adriano Ayala <adriano.ayc@gmail.com>
2022-06-07 15:37:36 -0400

    Solucionando el Problema de carga del Componente

--- 09-PROYECTO_AdministradorClientesFormikJsonSever/crm-react/src/paginas/VerCliente.jsx
+++ 09-PROYECTO_AdministradorClientesFormikJsonSever/crm-react/src/paginas/VerCliente.jsx
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom';
 const VerCliente = () => {
 
   const [cliente, setCliente] = useState({});
+  const [cargando, setCargando] = useState(false);
 
   // Obtenemos un objeto con el valor del parametro id
   // const params = useParams();
@@ -13,6 +14,9 @@ const VerCliente = () => {
 
   // Se ejecuta solo una vez
   useEffect(() => {
+    // Establecemos el opuesto al valor cargando
+    setCargando(!cargando);
+
     const obtenerClienteAPI = async () => {
       try {
         const url = `http://localhost:4000/clientes/${id}`;
@@ -27,42 +31,49 @@ const VerCliente = () => {
       } catch(error) {
         console.log(error);
       }
+
+      setCargando(false);
     }
 
     // Llamar el metodo
     obtenerClienteAPI();
 
   }, []);
+  // console.log(cargando);
   
   return (
     <div>
-      <h1 className='font-black text-4xl text-blue-900'>Ver Cliente: {cliente.nombre}</h1>
-      <p className='mt-3'>Información del Cliente</p>
-
-      {cliente.nombre && (
-        <p className='text-4xl text-gray-600 mt-10'>
-          <span className='text-gray-800 uppercase font-bold'>Cliente: </span>{cliente.nombre}
-        </p>
-      )}
-      {cliente.email && (
-        <p className='text-2xl text-gray-600 mt-4'>
-          <span className='text-gray-800 uppercase font-bold'>Email: </span>{cliente.email}
-        </p>
-      )}
-      {cliente.telefono && (
-        <p className='text-2xl text-gray-600 mt-4'>
-          <span className='text-gray-800 uppercase font-bold'>Teléfono: </span>{cliente.telefono}
-        </p>
-      )}
-      {cliente.empresa &&(
-        <p className='text-2xl text-gray-600 mt-4'>
-          <span className='text-gray-800 uppercase font-bold'>Empresa: </span>{cliente.empresa}
-        </p>
-      )}
-      {cliente.notas && (
-        <p className='text-2xl text-gray-600 mt-4'>
-          <span className='text-gray-800 uppercase font-bold'>Notas: </span>{cliente.notas}
-        </p>
+      {cargando ? 'Cargando...' : (
+        <>
+          <h1 className='font-black text-4xl text-blue-900'>Ver Cliente: {cliente.nombre}</h1>
+          <p className='mt-3'>Información del Cliente</p>
+    
+          {cliente.nombre && (
+            <p className='text-4xl text-gray-600 mt-10'>
+              <span className='text-gray-800 uppercase font-bold'>Cliente: </span>{cliente.nombre}
+            </p>
+          )}
+          {cliente.email && (
+            <p className='text-2xl text-gray-600 mt-4'>
+              <span className='text-gray-800 uppercase font-bold'>Email: </span>{cliente.email}
+            </p>
+          )}
+          {cliente.telefono && (
+            <p className='text-2xl text-gray-600 mt-4'>
+              <span className='text-gray-800 uppercase font-bold'>Teléfono: </span>{cliente.telefono}
+            </p>
+          )}
+          {cliente.empresa &&(
+            <p className='text-2xl text-gray-600 mt-4'>
+              <span className='text-gray-800 uppercase font-bold'>Empresa: </span>{cliente.empresa}
+            </p>
+          )}
+          {cliente.notas && (
+            <p className='text-2xl text-gray-600 mt-4'>
+              <span className='text-gray-800 uppercase font-bold'>Notas: </span>{cliente.notas}
+            </p>
+          )}
+        </>
       )}
     </div>
   )
