4841e0609412f70ce1f8e01fb1d7fda9a4b501cc
Adriano Ayala <adriano.ayc@gmail.com>
2022-07-19 22:53:26 -0400

    Finalizando el CSS al Header

--- 10-GuitarLA-ProyectoVentaGuitarrasNextJsStrapi/guitarla_nextjs/components/Header.js
+++ 10-GuitarLA-ProyectoVentaGuitarrasNextJsStrapi/guitarla_nextjs/components/Header.js
@@ -7,18 +7,20 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <div className="contenedor">
-        <Link href="/">
-          {/* fill - toma al ancho de la pantalla */}
-          {/* <Image layout="fill" src="/img/logo.svg" /> */}
-          <Image width={400} height={100} src="/img/logo.svg" alt="Imagen logo" /> 
-        </Link>
+        <div className={styles.barra}>
+          <Link href="/">
+            {/* fill - toma al ancho de la pantalla */}
+            {/* <Image layout="fill" src="/img/logo.svg" /> */}
+            <Image width={400} height={100} src="/img/logo.svg" alt="Imagen logo" /> 
+          </Link>
 
-        <nav>
-          <Link href="/">Inicio</Link>
-          <Link href="/nosotros">Nosotros</Link>
-          <Link href="/blog">Blog</Link>
-          <Link href="/tienda">Tienda</Link>
-        </nav>
+          <nav className={styles.navegacion}>
+            <Link href="/">Inicio</Link>
+            <Link href="/nosotros">Nosotros</Link>
+            <Link href="/blog">Blog</Link>
+            <Link href="/tienda">Tienda</Link>
+          </nav>
+        </div>
       </div>
     </header>
   )
