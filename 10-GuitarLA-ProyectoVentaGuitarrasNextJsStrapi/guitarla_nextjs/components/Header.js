199e1163941bb930b17275e34c6da4c1d181af90
Adriano Ayala <adriano.ayc@gmail.com>
2022-07-19 22:28:40 -0400

    Modulos CSS y estilos al header

--- 10-GuitarLA-ProyectoVentaGuitarrasNextJsStrapi/guitarla_nextjs/components/Header.js
+++ 10-GuitarLA-ProyectoVentaGuitarrasNextJsStrapi/guitarla_nextjs/components/Header.js
@@ -1,14 +1,22 @@
 import Link from "next/link"
+// Importar modulo de CSS
+import styles from "../styles/Header.module.css"
 
 const Header = () => {
   return (
-    <header>
+    <header className={styles.header}>
+      {/* El estilo modifica al elemento */}
+      {/* <p>Hola Mundo!</p> */}
+
       <div className="contenedor">
         <div>
 
         </div>
 
-        <nav>
+        {/* El estilos de la navegacion no lo afecta al elemento */}
+        {/* <a href="#">Otro Enlace</a> */}
+
+        <nav className={styles.navegacion}>
           <Link href="/">Inicio</Link>
           <Link href="/nosotros">Nosotros</Link>
           <Link href="/blog">Blog</Link>
