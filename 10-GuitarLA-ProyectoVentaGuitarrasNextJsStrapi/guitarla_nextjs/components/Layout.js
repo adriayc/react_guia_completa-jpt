7172ddce53e2e0130921c0d0f4f61dc0066be52c
Adriano Ayala <adriano.ayc@gmail.com>
2022-07-20 17:03:37 -0400

    Creando la pagina de Nosotros

--- 10-GuitarLA-ProyectoVentaGuitarrasNextJsStrapi/guitarla_nextjs/components/Layout.js
+++ 10-GuitarLA-ProyectoVentaGuitarrasNextJsStrapi/guitarla_nextjs/components/Layout.js
@@ -14,7 +14,6 @@ const Layout = ({children, pagina}) => {
 
       <Header />
 
-      <h1>Desde Layout</h1>
       {children}
 
       <Footer />
--- 10-GuitarLA-ProyectoVentaGuitarrasNextJsStrapi/guitarla_nextjs/pages/nosotros.js
+++ 10-GuitarLA-ProyectoVentaGuitarrasNextJsStrapi/guitarla_nextjs/pages/nosotros.js
@@ -1,3 +1,6 @@
+import Image from 'next/image'
+// Importar modulo de CSS
+import styles from "../styles/Nosotros.module.css"
 // Importar componentes
 import Layout from '../components/Layout'
 
@@ -11,7 +14,28 @@ const Nosotros = () => {
     <Layout
       pagina='Nosotros'
     >
-      <h1>Desde Nosotros</h1>
+      <main className='contenedor'>
+        <h2 className='heading'>Nosotros</h2>
+
+        <div className={styles.contenido}>
+          {/* layour: fill - Muestra la imagen en toda la pagina (No necesita ancho y alto) */}
+          {/* <Image layout='fill' src="/img/nosotros.jpg" alt='Imagen sobre nosotros' /> */}
+
+          {/* layour: responsive - Muestra la imagen de forma responsivo en toda la pagina */}
+          <Image layout='responsive' width={600} height={450} src="/img/nosotros.jpg" alt='Imagen sobre nosotros' />
+
+          {/* layour: intrinsic - Muestra la imagen o se ajusta al ancho del contenedor */}
+          {/* <Image layout='intrinsic' width={600} height={450} src="/img/nosotros.jpg" alt='Imagen sobre nosotros' /> */}
+
+           {/* layour: fixed - Muestra la imagen del mismo tamaño */}
+           {/* <Image layout='fixed' width={600} height={450} src="/img/nosotros.jpg" alt='Imagen sobre nosotros' /> */}
+
+          <div>
+            <p>Quisque et erat ac libero bibendum consectetur. Morbi lobortis vitae orci quis egestas. Duis egestas vel sapien a eleifend. Nullam pellentesque velit non dolor placerat, ac suscipit velit viverra. Quisque mollis, eros a feugiat aliquet, diam massa aliquam ligula, eget maximus turpis elit non erat. Sed quis ligula ac libero mattis interdum sit amet ut dui. Duis sapien orci, pellentesque sed tristique nec, sodales sed risus. Nulla aliquet odio vel lobortis tempor.</p>
+            <p>Quisque et erat ac libero bibendum consectetur. Morbi lobortis vitae orci quis egestas. Duis egestas vel sapien a eleifend. Nullam pellentesque velit non dolor placerat, ac suscipit velit viverra. Quisque mollis, eros a feugiat aliquet, diam massa aliquam ligula, eget maximus turpis elit non erat. Sed quis ligula ac libero mattis interdum sit amet ut dui. Duis sapien orci, pellentesque sed tristique nec, sodales sed risus. Nulla aliquet odio vel lobortis tempor.</p>
+          </div>
+        </div>
+      </main>
     </Layout>
   )
 }
