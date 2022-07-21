41886cc4e6cf81b2c01dac87a9f1dc658d216095
Adriano Ayala <adriano.ayc@gmail.com>
2022-07-21 17:15:58 -0400

    Consultando la API para obtener la informacion de la entrada de Blog

--- 10-GuitarLA-ProyectoVentaGuitarrasNextJsStrapi/guitarla_nextjs/pages/blog/[id].js
+++ 10-GuitarLA-ProyectoVentaGuitarrasNextJsStrapi/guitarla_nextjs/pages/blog/[id].js
@@ -15,16 +15,60 @@ const EntradaBlog = ({entrada}) => {
 }
 
 // ServerSideProps - Recibe automaticamente la "query" de useRouter (Se muestra en server o terminal)
-export async function getServerSideProps({query: { id }}) {
+// export async function getServerSideProps({query: { id }}) {
 
-  // console.log(query)
+//   // console.log(query)
+//   // console.log(id)
+
+//   const url = `http://localhost:1337/api/blogs/${id}?fields=*&populate=imagen`
+
+//   const respuesta = await fetch(url)
+//   const { data } = await respuesta.json()
+//   console.log(data)
+
+//   return {
+//     props: {
+//       entrada: data
+//     }
+//   }
+// }
+
+
+// StaticPaths - Obtiene la rutas estaticas para construir
+export async function getStaticPaths() {
+
+  const url = `http://localhost:1337/api/blogs?fields=*&populate=imagen`
+
+  const respuesta = await fetch(url)
+  const { data } = await respuesta.json()
+  // console.log(data)
+
+  const paths = data.map(entrada => ({
+    params: { id: entrada.id.toString() } 
+  }))
+  // console.log(paths)
+  // console.log(paths[0].params.id)
+  // console.log(typeof paths[0].params.id)
+
+  // Retorna un objeto con los paths que recibe getStaticProps
+  return {
+    // paths: paths
+    paths,
+    // fallback: true            // Retorna una serie de rutas que se generan estaticamente (proyecto muy grande)
+    fallback: false        // Retorna todas las rutas (proyecto pequeño que no tengas muchas rutas)
+    // fallback: 'blocking'
+  }
+}
+
+// StaticProps - Requiere de StaticPaths en routing dinamicos para obtener la "paths" de la url (Se muestra en server o terminal)
+export async function getStaticProps({params: { id }}) {
   // console.log(id)
 
   const url = `http://localhost:1337/api/blogs/${id}?fields=*&populate=imagen`
 
   const respuesta = await fetch(url)
   const { data } = await respuesta.json()
-  console.log(data)
+  // console.log(data)
 
   return {
     props: {
