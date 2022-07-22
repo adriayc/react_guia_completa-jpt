3aa958d3b0afe821cc70c9b1e1ecd6746754e8d6
Adriano Ayala <adriano.ayc@gmail.com>
2022-07-22 11:58:36 -0400

    Consultar la API para obtener las guitarras

--- 10-GuitarLA-ProyectoVentaGuitarrasNextJsStrapi/guitarla_nextjs/pages/tienda.js
+++ 10-GuitarLA-ProyectoVentaGuitarrasNextJsStrapi/guitarla_nextjs/pages/tienda.js
@@ -1,14 +1,34 @@
 // Importar componentes
 import Layout from '../components/Layout'
 
-const Tienda = () => {
+const Tienda = ({guitarras}) => {
+
+  console.log(guitarras)
+
   return (
     <Layout
       pagina='Tienda Virtual'
     >
-      <h1>Desde Tienda</h1>
+      <main className='contenedor'>
+        <h1 className='heading'>Nuestra Colección</h1>
+      </main>
     </Layout>
   )
+}
+
+// ServerSideProps - Se obtiene la data en cada request (Se ejecuta en el lado del servidor o terminal)
+export async function getServerSideProps() {
+  const url = `${process.env.API_URL}/guitarras?fields=*&populate=imagen`
+  
+  const respuesta = await fetch(url)
+  const { data } = await respuesta.json()
+  // console.log(data)
+
+  return {
+    props: {
+      guitarras: data
+    }
+  }
 }
 
 export default Tienda
