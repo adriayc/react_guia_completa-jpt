3322afe6c08fa2bf8931023882f09a9a268c8ad1
Adriano Ayala <adriano.ayc@gmail.com>
2022-07-22 12:09:12 -0400

    Creando un componente para las guitarras

---
+++ 10-GuitarLA-ProyectoVentaGuitarrasNextJsStrapi/guitarla_nextjs/components/Listado.js
@@ -0,0 +1,13 @@
+
+const Listado = ({guitarras}) => {
+
+  console.log(guitarras)
+
+  return (
+    <div>
+      <h1>Desde Listado</h1>
+    </div>
+  )
+}
+
+export default Listado
--- 10-GuitarLA-ProyectoVentaGuitarrasNextJsStrapi/guitarla_nextjs/pages/tienda.js
+++ 10-GuitarLA-ProyectoVentaGuitarrasNextJsStrapi/guitarla_nextjs/pages/tienda.js
@@ -1,9 +1,10 @@
 // Importar componentes
 import Layout from '../components/Layout'
+import Listado from '../components/Listado'
 
 const Tienda = ({guitarras}) => {
 
-  console.log(guitarras)
+  // console.log(guitarras)
 
   return (
     <Layout
@@ -11,12 +12,19 @@ const Tienda = ({guitarras}) => {
     >
       <main className='contenedor'>
         <h1 className='heading'>Nuestra Colección</h1>
+
+        <Listado
+          guitarras={guitarras}
+        />
       </main>
     </Layout>
   )
 }
 
-// ServerSideProps - Se obtiene la data en cada request (Se ejecuta en el lado del servidor o terminal)
+/**
+ * ServerSideProps - Se obtiene la data en cada request (Se ejecuta en el lado del servidor o terminal) 
+ * Se ejecuta ServerSideProps y StaticProps solo en los archivos del directorio "pages" y no en los de componentes
+ */
 export async function getServerSideProps() {
   const url = `${process.env.API_URL}/guitarras?fields=*&populate=imagen`
   
