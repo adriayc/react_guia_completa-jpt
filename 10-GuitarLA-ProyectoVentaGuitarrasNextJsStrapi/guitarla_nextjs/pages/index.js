85ff1b2439afa4adf02e703543672737d4238a39
Adriano Ayala <adriano.ayc@gmail.com>
2022-07-28 11:42:45 -0400

    Multiples consultas en Nextjs

--- 10-GuitarLA-ProyectoVentaGuitarrasNextJsStrapi/guitarla_nextjs/pages/index.js
+++ 10-GuitarLA-ProyectoVentaGuitarrasNextJsStrapi/guitarla_nextjs/pages/index.js
@@ -2,9 +2,10 @@
 import Layout from '../components/Layout'
 import Listado from '../components/Listado'
 
-export default function Home({guitarras}) {
+export default function Home({guitarras, curso}) {
 
-  // console.log(guitarras)
+  console.log(guitarras)
+  console.log(curso)
 
   return (
     <Layout
@@ -23,14 +24,37 @@ export default function Home({guitarras}) {
 
 export async function getServerSideProps() {
   
-  const urlGuitarras = `${process.env.API_URL}/guitarras?fields=*&populate=imagen`
+  // Forma no optimizada para llamar multiples request (Llama a la request de guitarra y luego curso)
+  // const urlGuitarras = `${process.env.API_URL}/guitarras?fields=*&populate=imagen`
+  // const respuesta = await fetch(urlGuitarras)
+  // const { data } = await respuesta.json()
 
-  const respuesta = await fetch(urlGuitarras)
-  const { data } = await respuesta.json()
+  // const urlCurso = `${process.env.API_URL}/curso?fields=*&populate=imagen`
+  // const respuesta2 = await fetch(urlGuitarras)
+  // const { data: data2 } = await respuesta2.json()
+
+  // Forma optimizada de llamar multiple request (Llama de forma paralela)
+  const urlGuitarras = `${process.env.API_URL}/guitarras?fields=*&populate=imagen`
+  const urlCurso = `${process.env.API_URL}/curso?fields=*&populate=imagen`
+
+  // Llama de forma paralela los request de la urlGuitarras y urlCurso
+  const [resGuitarras, resCurso] = await Promise.all([
+    fetch(urlGuitarras),
+    fetch(urlCurso),
+  ])
+
+  const [{data: guitarras}, {data: curso}] = await Promise.all([
+    resGuitarras.json(),
+    resCurso.json(),
+  ])
+  // console.log(guitarras)
+  // console.log(curso)
 
   return {
     props: {
-      guitarras: data
+      // guitarras: guitarras
+      guitarras,
+      curso
     }
   }
 }
