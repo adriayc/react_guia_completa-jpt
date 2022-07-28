e1668ca5ca0610fae1c368883b7f28178181f592
Adriano Ayala <adriano.ayc@gmail.com>
2022-07-27 23:37:10 -0400

    Consultado Guitarras en la pagina de Inicio

--- 10-GuitarLA-ProyectoVentaGuitarrasNextJsStrapi/guitarla_nextjs/pages/index.js
+++ 10-GuitarLA-ProyectoVentaGuitarrasNextJsStrapi/guitarla_nextjs/pages/index.js
@@ -1,12 +1,36 @@
 // Importar componentes
 import Layout from '../components/Layout'
+import Listado from '../components/Listado'
 
-export default function Home() {
+export default function Home({guitarras}) {
+
+  // console.log(guitarras)
+
   return (
     <Layout
       pagina='Inicio'
     >
-      <h1>Desde Inicio</h1>
+      <main className='contenedor'>
+        <h1 className='heading'>Nuestra Colección</h1>
+
+        <Listado
+          guitarras={guitarras}
+        />
+      </main>
     </Layout>
   )
+}
+
+export async function getServerSideProps() {
+  
+  const urlGuitarras = `${process.env.API_URL}/guitarras?fields=*&populate=imagen`
+
+  const respuesta = await fetch(urlGuitarras)
+  const { data } = await respuesta.json()
+
+  return {
+    props: {
+      guitarras: data
+    }
+  }
 }
