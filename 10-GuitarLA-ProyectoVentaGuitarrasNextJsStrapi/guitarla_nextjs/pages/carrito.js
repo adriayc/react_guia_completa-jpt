96621ad7a2529fedffbdff1d8c6ccb08864ec6cf
Adriano Ayala <adriano.ayc@gmail.com>
2022-08-05 16:00:48 -0400

    Creando Pagina y Modulo de Carrito

---
+++ 10-GuitarLA-ProyectoVentaGuitarrasNextJsStrapi/guitarla_nextjs/pages/carrito.js
@@ -0,0 +1,21 @@
+// Importar modulo de CSS
+import styles from "../styles/Carrito.module.css"
+// Importar componentes
+import Layout from '../components/Layout'
+
+const Carrito = () => {
+  return (
+    <Layout
+      pagina={'Carrito de Compras'}
+    >
+      <h1 className='heading'>Carrito</h1>
+
+      <main className={`contenedor ${styles.contenido}`}>
+        <div>1</div>
+        <div>2</div>
+      </main>
+    </Layout>
+  )
+}
+
+export default Carrito
