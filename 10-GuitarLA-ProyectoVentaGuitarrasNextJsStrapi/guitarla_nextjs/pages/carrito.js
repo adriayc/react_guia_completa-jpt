061e99f89feb8f9f811af41182acb0972d056ee3
Adriano Ayala <adriano.ayc@gmail.com>
2022-08-17 21:54:48 -0400

    Primeros Pasos con la Pagina de Carrito

--- 10-GuitarLA-ProyectoVentaGuitarrasNextJsStrapi/guitarla_nextjs/pages/carrito.js
+++ 10-GuitarLA-ProyectoVentaGuitarrasNextJsStrapi/guitarla_nextjs/pages/carrito.js
@@ -1,3 +1,4 @@
+import Image from "next/image"
 // Importar modulo de CSS
 import styles from "../styles/Carrito.module.css"
 // Importar componentes
@@ -15,7 +16,24 @@ const Carrito = ({ carrito }) => {
       <h1 className='heading'>Carrito</h1>
 
       <main className={`contenedor ${styles.contenido}`}>
-        <div>1</div>
+        <div className={styles.carrito}>
+          {carrito.length === 0 ? 'Carrito Vacio' : (
+            carrito.map(producto => (
+              <div key={producto.id} className={styles.producto}>
+                <div>
+                  <Image layout="responsive" width={250} height={480} src={producto.imagen} alt={producto.nombre} />
+                </div>
+
+                <div>
+                  <p className={styles.nombre}>{producto.nombre}</p>
+                  <p className={styles.cantidad}>Cantidad: {producto.cantidad}</p>
+                  <p className={styles.precio}>$<span>{producto.precio}</span></p>
+                  <p className={styles.subtotal}>Subtotal: $<span>{producto.precio * producto.cantidad}</span></p>
+                </div>
+              </div>
+            ))
+          )}
+        </div>
         <div>2</div>
       </main>
     </Layout>
