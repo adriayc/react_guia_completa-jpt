904a5dfd53b8e18a4a2e2526659d849d15b7e8b9
Adriano Ayala <adriano.ayc@gmail.com>
2022-08-22 18:37:32 -0400

    Añadiendo el Formulario

--- 14-PROYECTO-BuscadorLetrasInfoArtistas/letrasmusica/src/components/Formulario.jsx
+++ 14-PROYECTO-BuscadorLetrasInfoArtistas/letrasmusica/src/components/Formulario.jsx
@@ -1,6 +1,46 @@
+import { useState } from "react"
+
 const Formulario = () => {
+
+  const [busqueda, setBusqueda] = useState({
+    artista: '',
+    cancion: ''
+  });
+
   return (
-    <div>Formulario</div>
+    <form>
+      <legend>Buscar por Artista y Canción</legend>
+
+      <div className="form-grid">
+        <div>
+          <label htmlFor="">Artista</label>
+          <input 
+            type="text"
+            name="artista"
+            placeholder="Nombre Artista"
+            value={busqueda.artista}
+            onChange={e => setBusqueda({
+              ...busqueda,
+              [e.target.name]: e.target.value
+            })} />
+        </div>
+
+        <div>
+          <label htmlFor="">Canción</label>
+          <input 
+            type="text"
+            name="cancion"
+            placeholder="Nombre Canción"
+            value={busqueda.cancion}
+            onChange={e => setBusqueda({
+              ...busqueda,
+              [e.target.name]: e.target.value
+            })} />
+        </div>
+
+        <input type="submit" value="Buscar" />
+      </div>
+    </form>
   )
 }
 
