f68e9497038853ffdc477435466525b6119734be
Adriano Ayala <adriano.ayc@gmail.com>
2022-08-22 16:48:37 -0400

    Finalizando el Componente de Resultado

--- 11-PROYECTO-CotizadorSegurosAutomovilContextApi/cotizador/src/components/Resultado.jsx
+++ 11-PROYECTO-CotizadorSegurosAutomovilContextApi/cotizador/src/components/Resultado.jsx
@@ -14,6 +14,7 @@ const Resultado = () => {
   // Destructuring de un array
   const [nombreMarca] = MARCAS.filter(m => m.id === Number(marca));
   // console.log(nombreMarca);
+  const [nombrePlan] = PLANES.filter(p => p.id === Number(plan));
 
   if(resultado === 0) return null;
 
@@ -24,6 +25,21 @@ const Resultado = () => {
       <p className="my-2">
         <span className="font-bold">Marca: </span>
         {nombreMarca.nombre}
+      </p>
+
+      <p className="my-2">
+        <span className="font-bold">Plan: </span>
+        {nombrePlan.nombre}
+      </p>
+
+      <p className="my-2">
+        <span className="font-bold">Año del Auto: </span>
+        {year}
+      </p>
+
+      <p className="my-2 text-2xl">
+        <span className="font-bold">Total Cotización: </span>
+        {resultado}
       </p>
     </div>
   )
