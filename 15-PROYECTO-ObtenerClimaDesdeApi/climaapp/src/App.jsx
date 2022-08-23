b989b869eb55a119dbe8aaa134179ebfa5b2316a
Adriano Ayala <adriano.ayc@gmail.com>
2022-08-23 12:19:53 -0400

    Creando el Provider

--- 15-PROYECTO-ObtenerClimaDesdeApi/climaapp/src/App.jsx
+++ 15-PROYECTO-ObtenerClimaDesdeApi/climaapp/src/App.jsx
@@ -1,10 +1,18 @@
+// Importar el provider de clima
+import { ClimaProvider } from "./context/ClimaProvider";
 // Importar componentes
 import AppClima from "./components/AppClima";
 
 function App() {
 
   return (
-    <AppClima />
+    <ClimaProvider>
+      <header>
+        <h1>Buscador de Clima</h1>
+      </header>
+
+      <AppClima />
+    </ClimaProvider>
   )
 }
 
