be6e81e3bd64cdcb4567c92d6c07c55024380320
Adriano Ayala <adriano.ayc@gmail.com>
2022-08-24 17:19:59 -0400

    Creando el Context

--- 16-PROYECTO-BuscadorNoticiasApi/noticiasapp/src/App.jsx
+++ 16-PROYECTO-BuscadorNoticiasApi/noticiasapp/src/App.jsx
@@ -1,3 +1,5 @@
+// Importar el provedor de noticas
+import { NoticiasProvider } from './context/NoticiasProvider';
 // Importar componentes de MUI
 // OPCION #1 de importar (recomendada por MUI)
 // import Container from '@mui/material/Container';
@@ -10,32 +12,35 @@ import Formulario from './components/Formulario';
 function App() {
 
   return (
-    <Container>
-      <header>
-        {/* Props como marginY no esta definidas */}
-        {/* <Typography align='center' marginY={15}>Buscador de Noticias</Typography> */}
+    // Rodeamos el container con componente de Provider
+    <NoticiasProvider>
+      <Container>
+        <header>
+          {/* Props como marginY no esta definidas */}
+          {/* <Typography align='center' marginY={15}>Buscador de Noticias</Typography> */}
 
-        {/* Props xs para personalizar con CSS */}
-        {/* <Typography align='center' sx={{
-          fontWeight: 'bold'
-        }}>Buscador de Noticias</Typography> */}
+          {/* Props xs para personalizar con CSS */}
+          {/* <Typography align='center' sx={{
+            fontWeight: 'bold'
+          }}>Buscador de Noticias</Typography> */}
 
-        <Typography align='center' marginY={5} component='h1' variant='h3'>Buscador de Noticias</Typography>
-      </header>
+          <Typography align='center' marginY={5} component='h1' variant='h3'>Buscador de Noticias</Typography>
+        </header>
 
-      {/* Agregamos un Grid */}
-      <Grid 
-        container
-        direction='row'
-        justifyContent='center'
-        alignItems='center'
-      >
-        {/* Agregamos otro Grid con el prop item y establecemos media queries con sx, md y lg */}
-        <Grid item xs={12} md={6} lg={4}>
-          <Formulario />
+        {/* Agregamos un Grid */}
+        <Grid 
+          container
+          direction='row'
+          justifyContent='center'
+          alignItems='center'
+        >
+          {/* Agregamos otro Grid con el prop item y establecemos media queries con sx, md y lg */}
+          <Grid item xs={12} md={6} lg={4}>
+            <Formulario />
+          </Grid>
         </Grid>
-      </Grid>
-    </Container>
+      </Container>
+    </NoticiasProvider>
   )
 }
 
