7cd746f28f34584a7db6b07897c53e0175dfc217
Adriano Ayala <adriano.ayc@gmail.com>
2022-08-24 16:19:53 -0400

    Como utilizar los componentes de MUI

--- 16-PROYECTO-BuscadorNoticiasApi/noticiasapp/src/App.jsx
+++ 16-PROYECTO-BuscadorNoticiasApi/noticiasapp/src/App.jsx
@@ -1,7 +1,26 @@
+// Importar componentes de MUI
+// OPCION #1 de importar (recomendada por MUI)
+// import Container from '@mui/material/Container';
+// import Grid from '@mui/material/Grid';
+// OPCION #2 de importar
+import { Container, Grid, Typography } from '@mui/material';
+
 function App() {
 
   return (
-    <h1>App Noticias</h1>
+    <Container>
+      <header>
+        {/* Props como marginY no esta definidas */}
+        {/* <Typography align='center' marginY={15}>Buscador de Noticias</Typography> */}
+
+        {/* Props xs para personalizar con CSS */}
+        {/* <Typography align='center' sx={{
+          fontWeight: 'bold'
+        }}>Buscador de Noticias</Typography> */}
+
+        <Typography align='center' marginY={5} component='h1' variant='h3'>Buscador de Noticias</Typography>
+      </header>
+    </Container>
   )
 }
 
