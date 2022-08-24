2b69daa213eb75ebae8c6c645dc63c40e25f4d4d
Adriano Ayala <adriano.ayc@gmail.com>
2022-08-24 18:17:29 -0400

    Consultando las Noticias segun la categoria seleccionada

--- 16-PROYECTO-BuscadorNoticiasApi/noticiasapp/src/context/NoticiasProvider.jsx
+++ 16-PROYECTO-BuscadorNoticiasApi/noticiasapp/src/context/NoticiasProvider.jsx
@@ -1,14 +1,34 @@
-import { useState, createContext } from "react";
+import axios from "axios";
+import { useState, createContext, useEffect } from "react";
 
 const NoticiasContext = createContext();
 
 const NoticiasProvider = ({children}) => {
 
   const [categoria, setCategoria] = useState('general');
+  const [noticias, setNoticias] = useState([]);
 
   const handleChangeCategoria = e => {
     setCategoria(e.target.value);
   };
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      // Obtiene por defecto los 20 items de la 1ra pagina
+      const url = `https://newsapi.org/v2/top-headlines?country=us&category=${categoria}&apiKey=${import.meta.env.VITE_API_KEY}`;
+      // Obtiene el resto de los items de la 2da pagina
+      // const url = `https://newsapi.org/v2/top-headlines?country=us&category=${categoria}&page=2&apiKey=${import.meta.env.VITE_API_KEY}`;
+      // Obtiene los 100 items por pagina
+      // const url = `https://newsapi.org/v2/top-headlines?country=us&category=${categoria}&pageSize=100&apiKey=${import.meta.env.VITE_API_KEY}`;
+      // console.log(url);
+
+      const { data } = await axios(url);
+      // console.log(data.articles);
+
+      setNoticias(data.articles);
+    };
+    consultarAPI();
+  }, [categoria]);
 
   return (
     <NoticiasContext.Provider
