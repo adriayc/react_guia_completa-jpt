86b2cc0667ebe1d5b454f21b88ab64642f966b5f
Adriano Ayala <adriano.ayc@gmail.com>
2022-08-28 14:31:18 -0400

    Añadiendo los datos del Formulario al State y Validando

--- 17-PROYECTO-BuscadorBebidasContext/buscadorbebidas/src/components/Formulario.jsx
+++ 17-PROYECTO-BuscadorBebidasContext/buscadorbebidas/src/components/Formulario.jsx
@@ -1,15 +1,38 @@
+import { useState } from 'react';
 // Importar el custom hooks categorias
 import useCategorias from '../hooks/useCategorias';
 // Importar componentes react bootstrap
-import { Button, Form, Row, Col } from 'react-bootstrap'; 
+import { Button, Form, Row, Col, Alert } from 'react-bootstrap'; 
 
 const Formulario = () => {
 
+  const [busqueda, setBusqueda] = useState({
+    nombre: '',
+    categoria: ''
+  });
+  const [alerta, setAlerta] = useState('');
+
   const { categorias } = useCategorias();
-  console.log(categorias);
+  // console.log(categorias);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    if(Object.values(busqueda).includes('')) {
+      // console.log('Todos los campos son obligatorios');
+
+      setAlerta('Todos los campos son obligatorios');
+      return;
+    }
+    setAlerta('');
+  };  
 
   return (
-    <Form>
+    <Form
+      onSubmit={handleSubmit}
+    >
+      {alerta && <Alert variant='danger' className='text-center'>{alerta}</Alert>}
+
       <Row>
         <Col md={6}>
           <Form.Group className='mb-3'>
@@ -19,6 +42,11 @@ const Formulario = () => {
               name='nombre'
               id='nombre'
               placeholder='Ej: Tequila, Vodka, etc'
+              value={busqueda.nombre}
+              onChange={e => setBusqueda({
+                ...busqueda,
+                [e.target.name]: e.target.value 
+              })}
             />
           </Form.Group>
         </Col>
@@ -27,7 +55,12 @@ const Formulario = () => {
             <Form.Label htmlFor='categoria'>Categoría Bebida</Form.Label>
             <Form.Select
               name='categoria'
-              id='categoria'  
+              id='categoria'
+              value={busqueda.categoria}
+              onChange={e => setBusqueda({
+                ...busqueda,
+                [e.target.name]: e.target.value
+              })}
             >
               <option value="">- Selecciona Categoria -</option>
               {categorias.map(categoria => (
@@ -45,6 +78,7 @@ const Formulario = () => {
       <Row className='justify-content-end'>
         <Col md={3}>
           <Button
+            type='submit'
             variant='danger'
             className='text-uppercase w-100'
           >Buscar Bebidas</Button>
