9e1abb052e87c93ae88501981af0dbea18472fb6
Adriano Ayala <adriano.ayc@gmail.com>
2022-09-06 11:50:30 -0400

    Improve product update

--- 18-QuioscoApp_CreandoQuioscoComidaNextjsPrismaTailwind/quioscoapp/pages/index.js
+++ 18-QuioscoApp_CreandoQuioscoComidaNextjsPrismaTailwind/quioscoapp/pages/index.js
@@ -1,8 +1,33 @@
 import Head from 'next/head'
 import Image from 'next/image'
+// Importa Prisma Client
+import { PrismaClient } from '@prisma/client';
 
-export default function Home() {
+export default function Home({ categorias }) {
+  console.log(categorias);
+
   return (
     <h1>Next.js</h1>
   )
 }
+
+// Forma #1 - Consultar datos de prisma
+export const getServerSideProps = async () => {
+  const prisma = new PrismaClient;
+
+  const categorias = await prisma.categoria.findMany();  // Obtener todas las categorias
+  // const categorias = await prisma.categoria.findFirst(); // Obtener la prima categoria
+  // const categorias = await prisma.categoria.findFirst({     // Obtener la categoria con id = 3 o name = "Pizzas"
+  //   where: {
+  //     // id: 3
+  //     name: "Pizzas"
+  //   }
+  // });
+  // console.log(categorias);
+
+  return {
+    props: {
+      categorias
+    }
+  };
+};
