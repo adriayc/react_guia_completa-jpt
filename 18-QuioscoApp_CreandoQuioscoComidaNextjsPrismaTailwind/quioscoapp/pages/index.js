b99db1097b84c356d02e4043731c2c23a81eadbb
Adriano Ayala <adriano.ayc@gmail.com>
2022-10-13 23:32:43 -0400

    Creando el Layout Principal

--- 18-QuioscoApp_CreandoQuioscoComidaNextjsPrismaTailwind/quioscoapp/pages/index.js
+++ 18-QuioscoApp_CreandoQuioscoComidaNextjsPrismaTailwind/quioscoapp/pages/index.js
@@ -1,45 +1,13 @@
-// import { useEffect } from 'react';
 import Head from 'next/head'
 import Image from 'next/image'
-// Importa Prisma Client
-import { PrismaClient } from '@prisma/client';
 
-export default function Home({ categorias }) {
-  console.log(categorias);
+// Importar layour
+import Layout from '../layout/Layout'
 
-  // NOTA: Error, no se pude consultar prisma desde el cliente (seria inseguro)
-  // useEffect(() => {
-  //   const consultarDB = async () => {
-  //     const prisma = new PrismaClient();
-  //     const categorias = await prisma.categoria.findMany();
-  //     console.log(categorias);
-  //   }
-
-  //   consultarDB();
-  // }, [])
-
+export default function Home() {
   return (
-    <h1>Next.js</h1>
+    <Layout>
+      <h1>Inicio</h1>
+    </Layout>
   )
 }
-
-// Forma #1 - Consultar datos de prisma
-export const getServerSideProps = async () => {
-  const prisma = new PrismaClient;
-
-  const categorias = await prisma.categoria.findMany();  // Obtener todas las categorias
-  // const categorias = await prisma.categoria.findFirst(); // Obtener la prima categoria
-  // const categorias = await prisma.categoria.findFirst({     // Obtener la categoria con id = 3 o name = "Pizzas"
-  //   where: {
-  //     // id: 3
-  //     name: "Pizzas"
-  //   }
-  // });
-  // console.log(categorias);
-
-  return {
-    props: {
-      categorias
-    }
-  };
-};
--- 18-QuioscoApp_CreandoQuioscoComidaNextjsPrismaTailwind/quioscoapp/tailwind.config.js
+++ 18-QuioscoApp_CreandoQuioscoComidaNextjsPrismaTailwind/quioscoapp/tailwind.config.js
@@ -2,7 +2,9 @@
 module.exports = {
   // Configurar tailwindcss (archivos al que valos aplicar)
   content: [
-    "./pages**/*.{js,ts,jsx,tsx}"
+    "./pages**/*.{js,ts,jsx,tsx}",
+    "./layout**/*.{js,ts,jsx,tsx}",
+    "./components**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
