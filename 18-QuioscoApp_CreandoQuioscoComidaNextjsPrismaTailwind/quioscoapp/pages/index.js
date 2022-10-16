6daf1c9141a41cfc277f87cf243a6835bd6456cb
Adriano Ayala <adriano.ayc@gmail.com>
2022-10-15 22:56:00 -0400

    Mostrando los Productos de la Categoria actual

--- 18-QuioscoApp_CreandoQuioscoComidaNextjsPrismaTailwind/quioscoapp/pages/index.js
+++ 18-QuioscoApp_CreandoQuioscoComidaNextjsPrismaTailwind/quioscoapp/pages/index.js
@@ -6,6 +6,10 @@ import Layout from '../layout/Layout'
 // Importar custom hook
 import useQuiosco from '../hooks/useQuiosco'
 
+// Importar componentes
+import Producto from '../components/Producto'
+
+
 export default function Home() {
   const { categoriaActual } = useQuiosco()
 
@@ -15,6 +19,15 @@ export default function Home() {
       <p className='text-2xl my-10'>
         Elige y personaliza tu pedido
       </p>
+
+      <div className='grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4'>
+        {categoriaActual?.productos?.map(producto => (
+          <Producto
+            key={producto.id}
+            producto={producto}
+          />
+        ))}
+      </div>
     </Layout>
   )
 }
