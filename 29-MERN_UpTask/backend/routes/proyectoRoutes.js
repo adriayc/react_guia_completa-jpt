c3341c6cb8913b231955948796823dc745c70426
Adriano Ayala <adriano.ayc@gmail.com>
2022-11-12 23:16:22 -0400

    Creando el Routing y Funciones de Proyectos

---
+++ 29-MERN_UpTask/backend/routes/proyectoRoutes.js
@@ -0,0 +1,33 @@
+import express from "express"
+// Importar el middleware checkAuth
+import checkAuth from "../middleware/checkAuth.js"
+// Impotar controller
+import {
+    obtenerProyectos,
+    nuevoProyecto,
+    obtenerProyecto,
+    editarProyecto,
+    eliminarProyecto,
+    agregarColaborador,
+    eliminarColaborador,
+    obtenerTareas,
+} from '../controllers/proyectoController.js'
+
+const router = express.Router()
+
+// router.get('/', checkAuth, obtenerProyectos)
+// router.post('/', checkAuth, nuevoProyecto)
+router
+    .route('/')
+    .get(checkAuth, obtenerProyectos)
+    .post(checkAuth, nuevoProyecto)
+router
+    .route('/:id')
+    .get(checkAuth, obtenerProyecto)
+    .put(checkAuth, editarProyecto)
+    .delete(checkAuth, eliminarProyecto)
+router.get('/tareas/:id', obtenerTareas)
+router.post('/agregar-colaborador/:id', checkAuth, agregarColaborador)
+router.post('/eliminar-colaborador/:id', checkAuth, eliminarColaborador)
+
+export default router
