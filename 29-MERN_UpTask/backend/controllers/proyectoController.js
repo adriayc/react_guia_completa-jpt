2bbc03b5ae3ba17d59540c3caebab6da8bc2e905
Adriano Ayala <adriano.ayc@gmail.com>
2022-11-14 00:31:52 -0400

    Obtener y Validar un Proyecto por su ID

--- 29-MERN_UpTask/backend/controllers/proyectoController.js
+++ 29-MERN_UpTask/backend/controllers/proyectoController.js
@@ -1,4 +1,5 @@
 // Impotar modelos
+import mongoose from "mongoose"
 import Proyecto from "../models/Proyecto.js"
 
 const obtenerProyectos = async (req, res) => {
@@ -22,7 +23,37 @@ const nuevoProyecto = async (req, res) => {
     }
 }
 
-const obtenerProyecto = async (req, res) => {}
+const obtenerProyecto = async (req, res) => {
+    const { id } = req.params
+    // console.log(id)
+    // console.log(typeof id)
+    // console.log(mongoose.Types.ObjectId.isValid(id))
+
+    // Validar el ID en moogoose
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        const error = new Error('ID proyecto invalido')
+        return res.status(404).json({ msg: error.message })
+    }
+
+    const proyecto = await Proyecto.findById(id)
+    // console.log(proyecto)
+
+    if (!proyecto) {
+        const error = new Error('No encontrado')
+        return res.status(404).json({ msg: error.message })
+    }
+
+    // console.log(proyecto.creador)
+    // console.log(req.usuario._id)
+    // console.log(proyecto.creador.toString() === req.usuario._id.toString())   // convert ObjectId to string
+
+    if (proyecto.creador.toString() !== req.usuario._id.toString()) {
+        const error = new Error('Acción no válida')
+        return res.status(404).json({ msg: error.message })
+    }
+
+    return res.json(proyecto)
+}
 
 const editarProyecto = async (req, res) => {}
 
