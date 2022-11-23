db6cf59a8676d6cc2732ffaf2f365144708295a1
Adriano Ayala <adriano.ayc@gmail.com>
2022-11-23 17:44:34 -0400

    Validando el Formulario

--- 29-MERN_UpTask/frontend/src/pages/Registrar.jsx
+++ 29-MERN_UpTask/frontend/src/pages/Registrar.jsx
@@ -1,17 +1,41 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
+// Importar components
+import Alerta from '../components/Alerta'
 
 const Registrar = () => {
   const [ nombre, setNombre ] = useState('')
   const [ email, setEmail ] = useState('')
   const [ password, setPassword ] = useState('')
   const [ repetirPassword, setRepetirPassword ] = useState('')
+  const [ alerta, setAlerta ] = useState({}) 
+
+  const hadleSubmit = (e) => {
+    e.preventDefault()
+
+    if ([nombre, email, password, repetirPassword].includes('')) {
+      // console.log('Todos los campos son obligatorios')
+      setAlerta({
+        msg: 'Todos los campos son obligatorios',
+        error: true
+      })
+      return
+    }
+  }
+
+  const { msg } = alerta
 
   return (
     <>
       <h1 className='text-sky-600 font-black text-6xl capitalize'>Crea tu Cuenta y Administra tus <span className='text-slate-700'>proyectos</span></h1>
 
-      <form className='my-10 bg-white shadow rounded-lg p-10'>
+      {/* Mostrar la alerta */}
+      { msg && <Alerta alerta={alerta}/> }
+
+      <form 
+        className='my-10 bg-white shadow rounded-lg p-10'
+        onSubmit={hadleSubmit}
+      >
         <div className='my-5'>
           <label htmlFor="nombre" className='text-gray-600 text-xl font-bold uppercase block'>Nombre</label>
           <input 
