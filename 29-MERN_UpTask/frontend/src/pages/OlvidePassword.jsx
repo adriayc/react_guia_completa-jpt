ee28506956dd1d9139314fdad89c0460a3819f42
Adriano Ayala <adriano.ayc@gmail.com>
2022-11-24 17:44:51 -0400

    Primeros pasos con Reestablecer Password

--- 29-MERN_UpTask/frontend/src/pages/OlvidePassword.jsx
+++ 29-MERN_UpTask/frontend/src/pages/OlvidePassword.jsx
@@ -1,11 +1,37 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
+// Importar components
+import Alerta from '../components/Alerta'
 
 const OlvidePassword = () => {
+  const [ email, setEmail ] = useState('')
+  const [ alerta, setAlerta ] = useState({});
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    if (email === '' || email.length < 6) {
+      setAlerta({
+        msg: 'El Email es obligatorio',
+        error: true
+      })
+      return
+    }
+  }
+
+  const { msg } = alerta
+
   return (
     <>
     <h1 className='text-sky-600 font-black text-6xl capitalize'>Recupera tus acceso y no pierdas tus <span className='text-slate-700'>proyectos</span></h1>
 
-    <form className='my-10 bg-white shadow rounded-lg p-10'>
+    {/* Mostrar alerta */}
+    { msg && <Alerta alerta={alerta} /> }
+
+    <form 
+      className='my-10 bg-white shadow rounded-lg p-10'
+      onSubmit={handleSubmit}
+    >
       <div className="my-5">
         <label htmlFor="email" className="text-gray-600 text-xl font-bold block uppercase">Email</label>
         <input 
@@ -13,6 +39,8 @@ const OlvidePassword = () => {
           placeholder="Email de Registro"
           id="email"
           className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          value={email}
+          onChange={ e => setEmail(e.target.value) }
         />
       </div>
 
