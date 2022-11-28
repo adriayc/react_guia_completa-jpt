df136bbd36164c4343c795b675b625b0b2f09599
Adriano Ayala <adriano.ayc@gmail.com>
2022-11-28 15:46:01 -0400

    Creando el Context de Autenticacion

--- 29-MERN_UpTask/frontend/src/App.jsx
+++ 29-MERN_UpTask/frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 // Importar layouts
 import AuthLayout from './layouts/AuthLayout'
+// Importar providers
+import { AuthProvider } from './context/AuthProvider'
 // Importar pages
 import Login from './pages/Login'
 import Registrar from './pages/Registrar'
@@ -15,15 +17,17 @@ function App() {
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<AuthLayout />}>
-          <Route index element={<Login />} />
-          <Route path='registrar' element={<Registrar />} />
-          <Route path='olvide-password' element={<OlvidePassword />} />
-          <Route path='olvide-password/:token' element={<NuevoPassword />} />
-          <Route path='confirmar/:id' element={<ConfirmarCuenta />} />
-        </Route>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path='/' element={<AuthLayout />}>
+            <Route index element={<Login />} />
+            <Route path='registrar' element={<Registrar />} />
+            <Route path='olvide-password' element={<OlvidePassword />} />
+            <Route path='olvide-password/:token' element={<NuevoPassword />} />
+            <Route path='confirmar/:id' element={<ConfirmarCuenta />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   )
 }
