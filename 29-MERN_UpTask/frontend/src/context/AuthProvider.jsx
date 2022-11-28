e74ae30b8253c925a74133c6e716fb189dd8efb5
Adriano Ayala <adriano.ayc@gmail.com>
2022-11-28 16:21:40 -0400

    Revisando el Token para Autenticar al usuario

--- 29-MERN_UpTask/frontend/src/context/AuthProvider.jsx
+++ 29-MERN_UpTask/frontend/src/context/AuthProvider.jsx
@@ -1,9 +1,41 @@
 import { useState, useEffect, createContext } from "react"
+// Importar settings
+import clienteAxios from "../config/clienteAxios"
 
 const AuthContext = createContext()
 
 const AuthProvider = ({children}) => {
   const [ auth, setAuth ] = useState({})
+
+  useEffect(() => {
+    const autenticarUsuario = async () => {
+      const token = localStorage.getItem('token')
+      // console.log(token)
+
+      if (!token) {
+        return
+      }
+
+      // console.log('Si hay Token')
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`
+        }
+      }
+
+      try {
+        // Llamamos la API y le pasamos el config de headers
+        const { data } = await clienteAxios('/usuarios/perfil', config)
+
+        // console.log(data)
+        setAuth(data)
+      } catch (error) {
+        
+      }
+    }
+    autenticarUsuario()
+  }, [])
 
   return (
     <AuthContext.Provider
