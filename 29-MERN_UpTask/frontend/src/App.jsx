aba597d1f478735663f6b6b9a6c633793994c5a2
Adriano Ayala <adriano.ayc@gmail.com>
2022-12-15 12:23:31 -0400

    Añadiendo Routing para Editar un Proyecto

--- 29-MERN_UpTask/frontend/src/App.jsx
+++ 29-MERN_UpTask/frontend/src/App.jsx
@@ -15,6 +15,7 @@ import ConfirmarCuenta from './pages/ConfirmarCuenta'
 import Proyectos from './pages/Proyectos'
 import NuevoProyecto from './pages/NuevoProyecto'
 import Proyecto from './pages/Proyecto'
+import EditarProyecto from './pages/EditarProyecto'
 
 // Llamando a la variable de entorno 
 // console.log(import.meta.env.VITE_BACKEND_URL)
@@ -39,6 +40,7 @@ function App() {
               <Route index element={<Proyectos />} />
               <Route path='crear-proyecto' element={<NuevoProyecto />} />
               <Route path=':id' element={<Proyecto />} />    {/* Asegurarse que este al final porque tiene una ruta dinamica */}
+              <Route path='editar/:id' element={<EditarProyecto />} />
             </Route>
           </Routes>
         </ProyectosProvider>
