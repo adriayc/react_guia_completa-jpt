9d0c6942a8609fd94eaf1ceb92604ef041ed87df
Adriano Ayala <adriano.ayc@gmail.com>
2023-01-24 12:08:37 -0400

    Solucionar el Detalle con la Fecha

--- 29-MERN_UpTask/frontend/src/helpers/formatearFecha.jsx
+++ 29-MERN_UpTask/frontend/src/helpers/formatearFecha.jsx
@@ -1,6 +1,9 @@
 export const formatearFecha = fecha => {
-  const nuevaFecha = new Date(fecha)
+  // console.log(fecha)
+  // console.log(fecha.split('T')[0].split('-'))     // ['2023', '01', '31']
 
+  // const nuevaFecha = new Date(fecha)
+  const nuevaFecha = new Date(fecha.split('T')[0].split('-'))
   const opciones = {
     weekday: 'long',
     year: 'numeric',
