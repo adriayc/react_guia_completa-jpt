b8efacfd84b4e3578a5ef97ab114b6657c8a1e8b
Adriano Ayala <adriano.ayc@gmail.com>
2023-04-11 17:19:55 -0400

    Creando la Edicion de Productos y Listado

--- 56-CrudRedux/crudredux/src/components/EditarProducto.js
+++ 56-CrudRedux/crudredux/src/components/EditarProducto.js
@@ -1,6 +1,43 @@
 const EditarProducto = () => {
   return (
-    <h1>Desde EditarProducto.js</h1>
+    <div className="row justify-content-center">
+      <div className="col-md-8">
+        <div className="card">
+          <div className="card-body">
+            <h2 className="text-center mb-4 font-weight-bold">Editar Producto</h2>
+
+            <form>
+              <div className="form-group">
+                <label htmlFor="nombre" >Nombre Producto</label>
+                <input
+                  type="text"
+                  name="nombre"
+                  className="form-control"
+                  id="nombre"
+                  placeholder="Nombre Producto"
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="precio" >Precio Producto</label>
+                <input
+                  type="number"
+                  name="precio"
+                  className="form-control"
+                  id="precio"
+                  placeholder="Precio Producto"
+                />
+              </div>
+
+              <button
+                type="submit"
+                className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
+              >Guardar Cambios</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
  
