7dd90e1382f47e444f97773cfd6542938f9bae0a
Adriano Ayala <adriano.ayc@gmail.com>
2023-04-13 18:07:30 -0400

    Definiendo las primeras acciones de Redux

--- 56-CrudRedux/crudredux/src/reducers/index.js
+++ 56-CrudRedux/crudredux/src/reducers/index.js
@@ -4,5 +4,5 @@ import productosReducer from './productosReducer';
 
 // Si se requiere mas de 2 reducer usar combineReducers
 export default combineReducers({
-    productos: productosReducer,
+  productos: productosReducer,
 });
