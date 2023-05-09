dc1a9b414b7461b30ca09a689baefccd7f6acc00
Adriano Ayala <adriano.ayc@gmail.com>
2023-05-09 10:36:42 -0400

    Agregar un Nuevo Producto al State

--- 56-CrudRedux/crudredux/src/actions/productoActions.js
+++ 56-CrudRedux/crudredux/src/actions/productoActions.js
@@ -6,7 +6,31 @@ import {
 
   // Crear nuevos productos
   export function crearNuevoProductosAction(producto) {
-    return () => {
-      console.log(producto);
+    // dispatch - siempre mandan a llamar las acciones (en este caso son funciones)
+    return (dispatch) => {
+      // console.log(producto);
+
+      dispatch(agregarProducto());
+
+      try {
+        dispatch(agregarProductoExito(producto));
+      } catch (error) {
+        dispatch(agregarProductoError(true));
+      }
     };
   }
+
+  const agregarProducto = () => ({
+    type: AGREGAR_PRODUCTO,
+    // payload: // Modifica los datos (state) - no modificamos el state
+    payload: true
+  });
+
+  // Si el producto se guarda en la base de datos
+  const agregarProductoExito = producto => ({
+    type: AGREGAR_PRODUCTO_EXITO,
+    payload: producto     // Agregamos el paypload - modificamos el state
+  });
+
+  // Si hubo un error
+  const agregarProductoError = () => ({})
