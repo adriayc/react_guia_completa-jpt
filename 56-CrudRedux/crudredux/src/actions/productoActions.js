5f7129608ef0b744caa1c15277b68710cb5abc79
Adriano Ayala <adriano.ayc@gmail.com>
2023-06-14 09:55:24 -0400

    Configurando el Action

--- 56-CrudRedux/crudredux/src/actions/productoActions.js
+++ 56-CrudRedux/crudredux/src/actions/productoActions.js
@@ -1,63 +1,79 @@
 import {
     AGREGAR_PRODUCTO,
     AGREGAR_PRODUCTO_EXITO,
-    AGREGAR_PRODUCTO_ERROR
+    AGREGAR_PRODUCTO_ERROR,
+    COMENZAR_DESCARGA_PRODUCTOS,
+    DESCARGA_PRODUCTOS_EXITO,
+    DESCARGA_PRODUCTOS_ERROR
   } from '../types';
 // Importar axios
 import clienteAxios from '../config/axios';
 // Importar SweetAlert2
 import Swal from 'sweetalert2';
 
-  // Crear nuevos productos
-  export function crearNuevoProductosAction(producto) {
-    // dispatch - siempre mandan a llamar las acciones (en este caso son funciones)
-    return async (dispatch) => {
-      // console.log(producto);
+// Crear nuevos productos
+export function crearNuevoProductosAction(producto) {
+  // dispatch - siempre mandan a llamar las acciones (en este caso son funciones)
+  return async (dispatch) => {
+    // console.log(producto);
 
-      dispatch(agregarProducto());
+    dispatch(agregarProducto());
 
-      try {
-        // Insertar en la API
-        await clienteAxios.post('/productos', producto);
-        // Si todo sale bien, actualizar el state
-        dispatch(agregarProductoExito(producto));
+    try {
+      // Insertar en la API
+      await clienteAxios.post('/productos', producto);
+      // Si todo sale bien, actualizar el state
+      dispatch(agregarProductoExito(producto));
 
-        // Alerta de exito
-        Swal.fire(
-          'Correcto',
-          'El producto se agregó correctamente',
-          'success'
-        );
+      // Alerta de exito
+      Swal.fire(
+        'Correcto',
+        'El producto se agregó correctamente',
+        'success'
+      );
 
-      } catch (error) {
-        console.log(error);
-        // Si hay un error cambiar el state
-        dispatch(agregarProductoError(true));
+    } catch (error) {
+      console.log(error);
+      // Si hay un error cambiar el state
+      dispatch(agregarProductoError(true));
 
-        // Alerta de error
-        Swal.fire({
-          icon: 'error',
-          title: 'Hubo un error',
-          text: 'Hubo un error, intenta de nuevo'
-        });
-      }
-    };
-  }
+      // Alerta de error
+      Swal.fire({
+        icon: 'error',
+        title: 'Hubo un error',
+        text: 'Hubo un error, intenta de nuevo'
+      });
+    }
+  };
+}
 
-  const agregarProducto = () => ({
-    type: AGREGAR_PRODUCTO,
-    // payload: // Modifica los datos (state) - no modificamos el state
-    payload: true
-  });
+const agregarProducto = () => ({
+  type: AGREGAR_PRODUCTO,
+  // payload: // Modifica los datos (state) - no modificamos el state
+  payload: true
+});
 
-  // Si el producto se guarda en la base de datos
-  const agregarProductoExito = producto => ({
-    type: AGREGAR_PRODUCTO_EXITO,
-    payload: producto     // Agregamos el paypload - modificamos el state
-  });
+// Si el producto se guarda en la base de datos
+const agregarProductoExito = producto => ({
+  type: AGREGAR_PRODUCTO_EXITO,
+  payload: producto     // Agregamos el paypload - modificamos el state
+});
 
-  // Si hubo un error
-  const agregarProductoError = estado => ({
-    type: AGREGAR_PRODUCTO_ERROR,
-    payload: estado
-  })
+// Si hubo un error
+const agregarProductoError = estado => ({
+  type: AGREGAR_PRODUCTO_ERROR,
+  payload: estado
+})
+
+
+// Obtener productos (desde la DB)
+export function obtenerProductosAction() {
+  return async (dispatch) => {
+    dispatch(descargarProductos());
+  };
+}
+
+const descargarProductos = () => ({
+  type: COMENZAR_DESCARGA_PRODUCTOS,
+  payload: true
+});
--- 56-CrudRedux/crudredux/src/reducers/productosReducer.js
+++ 56-CrudRedux/crudredux/src/reducers/productosReducer.js
@@ -1,7 +1,10 @@
 import {
   AGREGAR_PRODUCTO,
   AGREGAR_PRODUCTO_EXITO,
-  AGREGAR_PRODUCTO_ERROR
+  AGREGAR_PRODUCTO_ERROR,
+  COMENZAR_DESCARGA_PRODUCTOS,
+  DESCARGA_PRODUCTOS_EXITO,
+  DESCARGA_PRODUCTOS_ERROR
 } from '../types';
 
 // Cada reducer tiene su propio state
@@ -14,6 +17,7 @@ const initialState = {
 export default function(state = initialState, action) {
   switch(action.type) {
     case AGREGAR_PRODUCTO:
+    case COMENZAR_DESCARGA_PRODUCTOS:
       return {
         ...state,
         // loading: true
@@ -25,13 +29,17 @@ export default function(state = initialState, action) {
         loading: false,
         productos: [...state.productos, action.payload]     // Actualizamos productos
       };
-
     case AGREGAR_PRODUCTO_ERROR:
       return {
         ...state,
         loading: false,
         error: action.payload
       };
+    // case COMENZAR_DESCARGA_PRODUCTOS:
+    //   return {
+    //     ...state,
+    //     loading: action.payload   // true
+    //   };
 
     default:
       return state;
