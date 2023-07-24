bc69e09979346c7cda23d774d480d3bfc8dc940f
Adriano Ayala <adriano.ayc@gmail.com>
2023-07-24 18:36:47 -0400

    Creando un segundo reducer

--- 56-CrudRedux/crudredux/src/reducers/index.js
+++ 56-CrudRedux/crudredux/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 // Impotar reducers
 import productosReducer from './productosReducer';
+import alertaReducer from './alertaReducer';
 
 // Si se requiere mas de 2 reducer usar combineReducers
 export default combineReducers({
   productos: productosReducer,
+  alerta: alertaReducer,
 });
