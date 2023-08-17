754a88b6c87bb3e057b8ca686b96186c63c92c8f
Adriano Ayala <adriano.ayc@gmail.com>
2023-08-17 00:49:10 -0400

    Agregando Fuentes de Google Fonts y Mas

--- 62-ProductHuntApp_NextJsFirebase/producthunt/components/layout/Layout.js
+++ 62-ProductHuntApp_NextJsFirebase/producthunt/components/layout/Layout.js
@@ -4,6 +4,8 @@ import React from 'react';
 import { Global, css } from '@emotion/react'; 
 // Importar routing
 import Link from 'next/link';
+// Importar head
+import Head from 'next/head';
 // Importar components
 import Header from './Header';
 
@@ -26,12 +28,20 @@ const Layout = props => {
             box-sizing: inherit;
           }
           body {
+            font-family: 'PT Sans', sans-serif;
             font-size: 1.6rem;    /* 16px */
             line-height: 1.5;
           }
           h1, h2, g3 {
             margin: 0 0 2rem 0;
             line-height: 1.5;
+          }
+          h1, h2 {
+            font-family: 'Roboto Slab', serif;
+            font-weight: 700;
+          }
+          h3 {
+            font-family: 'PT Sans', sans-serif;
           }
           ul {
             list-style: none;
@@ -43,6 +53,15 @@ const Layout = props => {
           }
         `}
       />
+
+      <Head>
+        <html lang="es" />
+        <title>Product Hunt Firebase y Next.js</title>
+        {/* Normalize */}
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        {/* Custom styles */}
+        <link href='/static/css/app.css' rel={'stylesheet'} />
+      </Head>
 
       <Header />
 
