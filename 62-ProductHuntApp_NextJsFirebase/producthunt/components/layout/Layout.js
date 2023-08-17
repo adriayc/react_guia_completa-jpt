c7748e614825f0df9b54412d462e634cd3668887
Adriano Ayala <adriano.ayc@gmail.com>
2023-08-16 23:08:34 -0400

    Agregando CSS Global

--- 62-ProductHuntApp_NextJsFirebase/producthunt/components/layout/Layout.js
+++ 62-ProductHuntApp_NextJsFirebase/producthunt/components/layout/Layout.js
@@ -1,4 +1,7 @@
 import React from 'react';
+// Importar emotion
+// import { Global, css } from '@emotion/core';   // Deprecado!
+import { Global, css } from '@emotion/react'; 
 // Importar routing
 import Link from 'next/link';
 // Importar components
@@ -7,6 +10,40 @@ import Header from './Header';
 const Layout = props => {
   return (
     <>
+      <Global
+        styles = {css`
+          :root {
+            --gris: #3d3d3d;
+            --gris2: #6f6f6f;
+            --naranja: #da552f;
+          }
+
+          html {
+            font-size: 62.5%;
+            box-sizing: border-box
+          }
+          *, *::before, *::after {
+            box-sizing: inherit;
+          }
+          body {
+            font-size: 1.6rem;    /* 16px */
+            line-height: 1.5;
+          }
+          h1, h2, g3 {
+            margin: 0 0 2rem 0;
+            line-height: 1.5;
+          }
+          ul {
+            list-style: none;
+            padding: 0;
+            margin: 0;
+          }
+          a {
+            text-decoration: none;
+          }
+        `}
+      />
+
       <Header />
 
       <main>
