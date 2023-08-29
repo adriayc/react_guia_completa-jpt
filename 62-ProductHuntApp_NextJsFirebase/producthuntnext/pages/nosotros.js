4b386cbfd3400dbbd5f97354607b7919ea100902
Adriano Ayala <adriano.ayc@gmail.com>
2023-08-29 10:27:32 -0400

    Routing en Next.js

---
+++ 62-ProductHuntApp_NextJsFirebase/producthuntnext/pages/nosotros.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// Importar layout component
+import Layout from '../components/layouts/Layout';
+
+const nosotros = () => {
+  return (
+    <div>
+      <Layout>
+        <h1>Nosotros</h1>
+      </Layout>
+    </div>
+  );
+}
+
+export default nosotros;
