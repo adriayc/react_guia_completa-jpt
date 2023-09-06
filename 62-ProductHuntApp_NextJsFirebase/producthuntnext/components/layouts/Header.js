b1d8f10a07404e1f67031207d5de4afde5e7a4e4
Adriano Ayala <adriano.ayc@gmail.com>
2023-09-06 10:55:26 -0400

    CSS al Header

--- 62-ProductHuntApp_NextJsFirebase/producthuntnext/components/layouts/Header.js
+++ 62-ProductHuntApp_NextJsFirebase/producthuntnext/components/layouts/Header.js
@@ -1,15 +1,50 @@
 import React from 'react';
 // Importar components
 import Buscar from '../ui/Buscar';
-import Navegacion from './Navegacion';
 import Link from 'next/link';
+// Impotar emotion
+import styled from '@emotion/styled';
+// import { css } from '@emotion/core';   // Deprecado! (Old version)
+import { css } from '@emotion/react';
+// Importar components
+import Navegacion from './Navegacion';
+
+// Styles
+const ContenedorHeader = styled.div`
+  width: 95%;
+  max-width: 1200px;
+  margin: 0 auto;
+  @media (min-width: 768px) {
+    display: flex;
+    justify-content: space-between;
+  }
+`;
+
+// const LogoParagraph = styled.(Link)`      // Error! en agregar style component a un componente (Permitido en gatsbyjs)
+const LogoParagraph = styled.p`
+  color: var(--naranja);
+  font-size: 4rem;
+  font-family: 'Roboto Slab', serif;
+  font-weight: 700;
+  line-height: 0;
+  margin-right: 2rem;
+  cursor: pointer;
+`;
 
 const Header = () => {
   return (
-    <header>
-      <div>
+    <header
+      css={css`
+        padding: 1rem 0;
+        border-bottom: 2px solid var(--gris3);
+      `}
+    >
+      <ContenedorHeader>
         <div>
-          <p>P</p>
+          {/* Insertar un style component dentro de un Link */}
+          <Link href='/'>
+            <LogoParagraph>P</LogoParagraph>
+          </Link>
 
           {/* Buscador aqui */}
           <Buscar />
@@ -27,7 +62,7 @@ const Header = () => {
           <Link href='/'>Login</Link>
           <Link href='/'>Crear Cuenta</Link>
         </div>
-      </div>
+      </ContenedorHeader>
     </header>
   );
 }
