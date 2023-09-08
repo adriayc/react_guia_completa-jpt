77cf9031706145b80b5ffa5046275f0266d3f051
Adriano Ayala <adriano.ayc@gmail.com>
2023-09-08 00:22:34 -0400

    Creando el Formulario para Crear Cuentas

--- 62-ProductHuntApp_NextJsFirebase/producthuntnext/pages/crear-cuenta.js
+++ 62-ProductHuntApp_NextJsFirebase/producthuntnext/pages/crear-cuenta.js
@@ -6,7 +6,43 @@ const CrearCuenta = () => {
   return (
     <div>
       <Layout>
-        <h1>Crear Cuenta</h1>
+        <>
+          <h1>Crear Cuenta</h1>
+
+          <form>
+            <div>
+              <label htmlFor='nombre'>Nombre</label>
+              <input
+                type='text'
+                id='nombre'
+                name='nombre'
+                placeholder='Tu Nombre'
+              />
+            </div>
+
+            <div>
+              <label htmlFor='email'>Email</label>
+              <input
+                type='email'
+                id='email'
+                name='email'
+                placeholder='Tu Email'
+              />
+            </div>
+
+            <div>
+              <label htmlFor='password'>Password</label>
+              <input
+                type='password'
+                id='password'
+                name='password'
+                placeholder='Tu Password'
+              />
+            </div>
+
+            <input type='submit' value='Crear Cuenta' />
+          </form>
+        </>
       </Layout>
     </div>
   );
