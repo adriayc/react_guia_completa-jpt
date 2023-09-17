03f90b1a5c34487cf42861ed6c08e43f2235cb2c
Adriano Ayala <adriano.ayc@gmail.com>
2023-09-17 16:12:54 -0400

    Consultando la Base de Datos

--- 62-ProductHuntApp_NextJsFirebase/producthuntnext/pages/index.js
+++ 62-ProductHuntApp_NextJsFirebase/producthuntnext/pages/index.js
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 // Impotar layout component
 import Layout from '../components/layouts/Layout';
+// Importar custom context firebase
+import { FirebaseContext } from '../firebase';
 
 const Home = () => {
+  const [productos, guardarProductos] = useState([]);
+
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    // Obtener los productos de firebase
+    const obtenerProductos = () => {
+      firebase
+        .db
+        .collection('productos')
+        .orderBy('creado', 'desc')
+        // Llamar un funcion snapshot
+        .onSnapshot(manejarSnapshot);
+    };
+
+    obtenerProductos();
+
+  }, []);
+
+  // Funcion snapshot para guardar los porductos en el state
+  function manejarSnapshot(snapshot) {
+    const productos = snapshot.docs.map(doc => {
+      return {
+        id: doc.id,
+        ...doc.data()
+      };
+    });
+
+    // console.log(productos);
+    guardarProductos(productos);
+  }
+
   return (
     <div>
       <Layout>
