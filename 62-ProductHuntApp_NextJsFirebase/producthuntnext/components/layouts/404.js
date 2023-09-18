0bc40500e5c8ee3c45f783c656e23998177ef56e
Adriano Ayala <adriano.ayc@gmail.com>
2023-09-18 18:56:53 -0400

    Validar que un producto exista

---
+++ 62-ProductHuntApp_NextJsFirebase/producthuntnext/components/layouts/404.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// Importar emotion
+// import { css } from '@emotion/core';
+import { css } from '@emotion/react';
+
+const Error404 = () => {
+  return (
+    <h1
+      css={css`
+        text-align: center;
+        margin-top: 5rem;
+      `}
+    >Producto no existente</h1>
+  );
+}
+
+export default Error404;
--- 62-ProductHuntApp_NextJsFirebase/producthuntnext/pages/productos/[id].js
+++ 62-ProductHuntApp_NextJsFirebase/producthuntnext/pages/productos/[id].js
@@ -1,22 +1,57 @@
-import React, { useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 // Importar routing
 import { useRouter } from 'next/router';
+// Importar el context de firebase
+import { FirebaseContext } from '../../firebase';
+// Impotar layout component
+import Layout from '../../components/layouts/Layout';
+// Importar components
+import Error404 from '../../components/layouts/404';
 
 const Producto = () => {
+  // State del componente
+  const [producto, guardarProducto] = useState({});
+  const [error, guardarError] = useState(false);
+
   // Obtener el id actual de la url
   const router = useRouter();
   const { query: { id } } = router;
    // console.log(router);
   // console.log(id);
 
+  // Context de firebase
+  const { firebase } = useContext(FirebaseContext);
+
   useEffect(() => {
     if (id) {
-      console.log('Existe un id:', id);
+      // console.log('Existe un id:', id);
+
+      const obtenerProducto = async () => {
+        const productoQuery = await firebase.db.collection('productos').doc(id);
+        const producto = await productoQuery.get();
+        // console.log(producto.data());
+        if (producto.exists) {
+          // console.log('Existe el producto');
+          guardarProducto(producto.data());
+        } else {
+          // console.log('No existe el producto');
+          guardarError(true);
+        }
+      };
+
+      obtenerProducto();
+
     }
   }, [id])
 
+  // if (error) return <Error404 />
+
   return (
-    <h1>Desde [id].js</h1>
+    <Layout>
+      <>
+        {error && <Error404 />}
+      </>
+    </Layout>
   );
 }
 
