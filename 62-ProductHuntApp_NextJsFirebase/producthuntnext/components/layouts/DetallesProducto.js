ae1c4367ecf3528d0e8f589d7100f5ad54077014
Adriano Ayala <adriano.ayc@gmail.com>
2023-09-18 08:32:09 -0400

    CSS al listado de Produtos

--- 62-ProductHuntApp_NextJsFirebase/producthuntnext/components/layouts/DetallesProducto.js
+++ 62-ProductHuntApp_NextJsFirebase/producthuntnext/components/layouts/DetallesProducto.js
@@ -5,8 +5,83 @@ import styled from '@emotion/styled';
 import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 import { es } from 'date-fns/locale';
 
+
+const Producto = styled.li`
+  padding: 4rem;
+  border-bottom: 1px solid #e1e1e1;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const DescripcionProducto = styled.div`
+  flex: 0 1 600px;
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  column-gap: 2rem;
+`;
+
 const Imagen = styled.img`
   width: 200px;
+`;
+
+const Titulo = styled.a`
+  font-size: 2rem;
+  font-weight: bold;
+  margin: 0;
+
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+const TextoDescripcion = styled.p`
+  color: #888;
+  font-size: 1.6rem;
+  margin: 0;
+`;
+
+const Comentarios = styled.div`
+  margin-top: 2rem;
+  display: flex;
+  align-items: center;
+
+  div {
+    padding: .3rem 1rem;
+    border: 1px solid #e1e1e1;
+    margin-right: 2rem;
+    display: flex;
+    align-items: center;
+  }
+  img {
+    width: 2rem;
+    margin-right: 2rem;
+  }
+  p {
+    font-size: 1.6rem;
+    font-weight: 700;
+    margin-right: 1rem;
+
+    &:last-of-type {
+      margin: 0;
+    }
+  }
+`;
+
+const Votos = styled.div`
+  flex: 0 0 auto;
+  text-align: center;
+  padding: 1rem 3rem;
+  border: 1px solid #e1e1e1;
+
+  div {
+    font-size: 2rem;
+  }
+  p {
+    font-size: 2rem;
+    font-weight: 700;
+    margin: 0;
+  }
 `;
 
 const DetallesProducto = ({producto}) => {
@@ -14,31 +89,33 @@ const DetallesProducto = ({producto}) => {
   const { id, nombre, url, urlImagen, descripcion, empresa, votos, comentarios, creado  } = producto;
 
   return (
-    <li>
-      <div>
+    <Producto>
+      <DescripcionProducto>
         <div>
           <Imagen src={urlImagen} />
         </div>
 
         <div>
-          <h1>{nombre}</h1>
+          <Titulo>{nombre}</Titulo>
 
-          <p>{descripcion}</p>
+          <TextoDescripcion>{descripcion}</TextoDescripcion>
 
-          <div>
-            <img src='/static/img/comentario.png' />
-            <p>{comentarios.length} Comentarios</p>
-          </div>
+          <Comentarios>
+            <div>
+              <img src='/static/img/comentario.png' />
+              <p>{comentarios.length} Comentarios</p>
+            </div>
+          </Comentarios>
 
           <p>Publicado hace: {formatDistanceToNow(new Date(creado), {locale: es})}</p>
         </div>
-      </div>
+      </DescripcionProducto>
 
-      <div>
+      <Votos>
         <div>&#9650;</div>
         <p>{votos}</p>
-      </div>
-    </li>
+      </Votos>
+    </Producto>
   );
 }
 
