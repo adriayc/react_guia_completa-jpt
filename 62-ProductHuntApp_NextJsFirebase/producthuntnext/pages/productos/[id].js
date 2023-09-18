f1cd94c973606ec8724e001d0ab50fdab81a5158
Adriano Ayala <adriano.ayc@gmail.com>
2023-09-18 18:31:41 -0400

    Primeros pasos con el Routing Dinamico

--- 62-ProductHuntApp_NextJsFirebase/producthuntnext/pages/productos/[id].js
+++ 62-ProductHuntApp_NextJsFirebase/producthuntnext/pages/productos/[id].js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+// Importar routing
+import { useRouter } from 'next/router';
 
 const Producto = () => {
+  // Obtener el id actual de la url
+  const router = useRouter();
+  const { query: { id } } = router;
+   // console.log(router);
+  // console.log(id);
+
+  useEffect(() => {
+    if (id) {
+      console.log('Existe un id:', id);
+    }
+  }, [id])
+
   return (
     <h1>Desde [id].js</h1>
   );
