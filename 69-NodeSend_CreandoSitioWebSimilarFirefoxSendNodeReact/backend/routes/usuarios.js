2295c2b41d0f25245a49d9ddb3abde5b2d82dacc
Adriano Ayala <adriano.ayc@gmail.com>
2023-10-07 21:51:35 -0400

    Probanndo el Endpoint de crear usuarios

--- 69-NodeSend_CreandoSitioWebSimilarFirefoxSendNodeReact/backend/routes/usuarios.js
+++ 69-NodeSend_CreandoSitioWebSimilarFirefoxSendNodeReact/backend/routes/usuarios.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+// Controllers
+const usuarioController = require('../controllers/usuarioController')
 
 router.post('/',
-
+    usuarioController.nuevoUsuario
 );
 
 module.exports = router;
