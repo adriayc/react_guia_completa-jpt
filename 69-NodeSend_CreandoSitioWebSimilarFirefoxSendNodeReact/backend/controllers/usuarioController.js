bfb17899fdacacc0d43e54813516906df449d22c
Adriano Ayala <adriano.ayc@gmail.com>
2023-10-07 22:29:00 -0400

    Como evitar registros duplicados

--- 69-NodeSend_CreandoSitioWebSimilarFirefoxSendNodeReact/backend/controllers/usuarioController.js
+++ 69-NodeSend_CreandoSitioWebSimilarFirefoxSendNodeReact/backend/controllers/usuarioController.js
@@ -5,7 +5,15 @@ exports.nuevoUsuario = async (req, res) => {
     // console.log('Desde nuevo usuarios');
     // console.log(req.body);
 
-    const usuario = await new Usuario(req.body);
+    // Verificar si el usuario ya estuvo registrado
+    const { email } = req.body;
+    
+    let usuario = await Usuario.findOne({ email });
+    if (usuario) {
+        return res.status(400).json({msg: 'El usuario ya esta registrado'});
+    }
+
+    usuario = await new Usuario(req.body);
     usuario.save();
 
     // res.json({msg: 'Usuario creado correctamente'});
