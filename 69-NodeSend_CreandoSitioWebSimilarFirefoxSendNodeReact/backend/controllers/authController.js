6ed6e12f24f4c036a19d838bb779716cffea2ad5
Adriano Ayala <adriano.ayc@gmail.com>
2023-10-09 23:50:04 -0400

    Verificar si un usuario existe o no

--- 69-NodeSend_CreandoSitioWebSimilarFirefoxSendNodeReact/backend/controllers/authController.js
+++ 69-NodeSend_CreandoSitioWebSimilarFirefoxSendNodeReact/backend/controllers/authController.js
@@ -1,4 +1,23 @@
+// Models
+const Usuario = require('../models/Usuario');
+
 exports.autenticarUsuario = async (req, res, next) => {
+    // Revisar si hay errores
+
+    // Buscar el usuario para ver si esta registrado
+    // console.log(req.body);
+    const { email } = req.body;
+    const usuario = await Usuario.findOne({ email });
+    // console.log(usuario);
+
+    if (!usuario) {
+        // res.status(401).json({msg: 'El usuario no existe'})
+        // next();         // Evita ejecutar las siguientes lineas (Tambien: return next())
+        return res.status(401).json({msg: 'El usuario no existe'});
+    }
+
+    // Verificar el password y autenticar el usuario
+    console.log('El usuario si existe');
 
 };
 
