7569a776ac9c5bc0e514e46efa57cf730e6befb4
Adriano Ayala <adriano.ayc@gmail.com>
2023-10-09 23:57:16 -0400

    Revisando si el password es correcto o no

--- 69-NodeSend_CreandoSitioWebSimilarFirefoxSendNodeReact/backend/controllers/authController.js
+++ 69-NodeSend_CreandoSitioWebSimilarFirefoxSendNodeReact/backend/controllers/authController.js
@@ -1,3 +1,5 @@
+// Bcrypt
+const bcrypt = require('bcrypt');
 // Models
 const Usuario = require('../models/Usuario');
 
@@ -6,7 +8,7 @@ exports.autenticarUsuario = async (req, res, next) => {
 
     // Buscar el usuario para ver si esta registrado
     // console.log(req.body);
-    const { email } = req.body;
+    const { email, password } = req.body;
     const usuario = await Usuario.findOne({ email });
     // console.log(usuario);
 
@@ -17,7 +19,17 @@ exports.autenticarUsuario = async (req, res, next) => {
     }
 
     // Verificar el password y autenticar el usuario
-    console.log('El usuario si existe');
+    // console.log('El usuario si existe');
+    if (bcrypt.compareSync(password, usuario.password)) {
+        // console.log('El password es correcto');
+        // Crear JWT
+
+    } else {
+        // console.log('El password es incorrecto');
+        // res.status(401).json({msg: 'El password es incorrecto'});
+        // return next();
+        return res.status(401).json({msg: 'El password es incorrecto'});
+    }
 
 };
 
