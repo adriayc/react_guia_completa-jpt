73160b8b67b3cb2247ca0b79b6d3dfa195e9e7ac
Adriano Ayala <adriano.ayc@gmail.com>
2023-10-14 22:59:58 -0400

    Definiendo Numero de Descargas y Passwords a Enlaces

--- 69-NodeSend_CreandoSitioWebSimilarFirefoxSendNodeReact/backend/controllers/enlacesController.js
+++ 69-NodeSend_CreandoSitioWebSimilarFirefoxSendNodeReact/backend/controllers/enlacesController.js
@@ -1,5 +1,7 @@
 // Shortid
 const shortid = require('shortid');
+// Bcrypt
+const bcrypt = require('bcrypt');
 // Models
 const Enlaces = require('../models/Enlace');
 
@@ -10,16 +12,33 @@ exports.nuevoEnlace = async (req, res, next) => {
 
     // Crear un objeto de Enlace
     // console.log(req.body);
-    const { nombre_original, password } = req.body;
+    const { nombre_original } = req.body;
 
     const enlace = new Enlaces();
     enlace.url = shortid.generate();
     enlace.nombre = shortid.generate();
     enlace.nombre_original = nombre_original;
-    enlace.password = password;
     // console.log(enlace);
 
     // Si el usuario esta autenticado
+    // console.log(req.usuario);
+    if (req.usuario) {
+        const { password, descargas } = req.body;
+
+        // Asignar a enlace el numero de descargas
+        if (descargas) {
+            enlace.descargas = descargas;
+        }
+        // Asignar un password
+        if (password) {
+            const salt = await bcrypt.genSalt(10);
+            // enlace.password = password;
+            enlace.password = await bcrypt.hash(password, salt);
+        }
+
+        // Asignar el autor
+        enlace.autor = req.usuario.id;
+    }
 
     // Almacenar en la BD
     try {
