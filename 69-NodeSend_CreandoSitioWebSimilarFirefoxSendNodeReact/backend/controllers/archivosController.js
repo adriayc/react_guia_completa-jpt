723703d91f4990fb646782cd6648b1b4ff7c0ebb
Adriano Ayala <adriano.ayc@gmail.com>
2023-10-16 18:51:42 -0400

    Creando una Configuracion mas avazada con Multer

--- 69-NodeSend_CreandoSitioWebSimilarFirefoxSendNodeReact/backend/controllers/archivosController.js
+++ 69-NodeSend_CreandoSitioWebSimilarFirefoxSendNodeReact/backend/controllers/archivosController.js
@@ -1,5 +1,44 @@
-exports.subirArchivo = async (req, res) => {
-    console.log(req.file);
+// Multer
+const multer = require('multer');
+// ShortId
+const shortid = require('shortid');
+
+// Configuarion de multer
+const configuracionMulter = {
+    // 1 MB de limite
+    // limits: { fileSize: 1000000 },
+    limits: { fileSize: 1024 * 1024 },
+    storage: fileStorage = multer.diskStorage({
+        destination: (req, res, cb) => {
+            cb(null, __dirname +'/../uploads');
+        },
+        filename: (req, file, cb) => {
+            const extension = file.mimetype.split('/')[1];
+            cb(null, `${shortid.generate()}.${extension}`);
+        },
+        // fileFilter: (req, file, cb) => {
+        //     //  Evitar archivos PDF's
+        //     if (file.mimetype === 'application/pdf') {
+        //         return cb(null, true);
+        //     }
+        // },
+    })
+};
+
+const upload = multer(configuracionMulter).single('archivo');
+
+exports.subirArchivo = async (req, res, next) => {
+    // console.log(req.file);
+    upload(req, res, async (error) => {
+        console.log(req.file);
+
+        if (!error) {
+            return res.json({archivo: req.file.filename});
+        } else {
+            console.log(error);
+            return next();
+        }
+    });
 };
 
 exports.eliminarArchivo = async (req, res) => {
--- 69-NodeSend_CreandoSitioWebSimilarFirefoxSendNodeReact/backend/routes/archivos.js
+++ 69-NodeSend_CreandoSitioWebSimilarFirefoxSendNodeReact/backend/routes/archivos.js
@@ -2,16 +2,10 @@ const express = require('express');
 const router = express.Router();
 // Middlewate
 const auth = require('../middleware/auth');
-// Multer
-const multer = require('multer');
 // Controllers
 const archivosController = require('../controllers/archivosController');
 
-// Subida de archivos (Con multer)
-const upload = multer({dest: './uploads/'});
-
 router.post('/',
-    upload.single('archivo'),
     archivosController.subirArchivo
 );
 
