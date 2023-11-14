4ede645d32c83c38d178ab26f40aa73f80ebd496
Adriano Ayala <adriano.ayc@gmail.com>
2023-11-13 23:58:33 -0400

    Primeros Pasos con Crear cuenta

--- 69-NodeSend_CreandoSitioWebSimilarFirefoxSendNodeReact/frontend/pages/crearcuenta.js
+++ 69-NodeSend_CreandoSitioWebSimilarFirefoxSendNodeReact/frontend/pages/crearcuenta.js
@@ -4,7 +4,28 @@ import Layout from "../components/Layout"
 const CrearCuenta = () => {
   return (
     <Layout>
-    <h1>Crear Cuenta</h1>
+      <div className="md:w-4/5 xl:w-3/5 mx-auto mb-32">
+        <h2 className="text-4xl font-sans font-bold text-gray-800 text-center my-4">Crear Cuenta</h2>
+
+        <div className="flex justify-center mt-5">
+          <div className="w-full max-w-lg">
+            <form className="bg-white roundend shadow-md px-8 pt-6 pb-8 mb-4">
+              <div className="mg-4">
+                <label 
+                  className="block text-black text-sm font-bold mb-2"
+                  htmlFor="nombre"
+                >Nombre</label>
+                <input
+                  type="text"
+                  className="shadow appearance-none border roundend w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="nombre"
+                  placeholder="Nombre de Usuario"
+                />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
   </Layout>
   )
 }
